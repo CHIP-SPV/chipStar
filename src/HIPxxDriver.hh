0f0aff20b691702bef53fe9c4d0ccb48cb2708c6
Paulius Velesko <paulius.velesko@intel.com>
2021-08-19 08:39:50 +0000

    Add HIPxxDriver.hh Doxygen

--- src/HIPxxDriver.hh
+++ src/HIPxxDriver.hh
@@ -5,10 +5,29 @@
 
 #include "HIPxxBackend.hh"
 #include "backend/backends.hh"
-
+/**
+ * @brief
+ * Global Backend pointer through which backend-specific operations are
+ * performed
+ */
 extern HIPxxBackend* Backend;
+
+/**
+ * @brief
+ * Singleton backend initialization flag
+ */
 extern std::once_flag initialized;
+
+/**
+ * @brief
+ * Singleton backend initialization function outer wrapper
+ */
 extern void initialize();
-extern void _initialize();
+
+/**
+ * @brief
+ * Singleton backend initialization function called via std::call_once
+ */
+void _initialize();
 
 #endif
