c6c10fc02abc97643c6016df1343280f83ffd16e
Paulius Velesko <paulius.velesko@intel.com>
2021-08-19 10:39:24 +0000

    HIPxxDriver.cc doc

--- src/HIPxxDriver.cc
+++ src/HIPxxDriver.cc
@@ -1,3 +1,15 @@
+/**
+ * @file HIPxxDriver.cc
+ * @author Paulius Velesko (pvelesko@gmail.com)
+ * @brief Definitions of extern declared functions and objects in HIPxxDriver.hh
+ * Initializing the HIPxx runtime with backend selection through HIPXX_BE
+ * environment variable.
+ * @version 0.1
+ * @date 2021-08-19
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #include "HIPxxDriver.hh"
 
 std::once_flag initialized;
