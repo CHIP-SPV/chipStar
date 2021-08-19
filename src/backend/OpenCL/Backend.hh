611dfb15668595e7e6918b165e5faed97574be2f
Paulius Velesko <paulius.velesko@intel.com>
2021-08-19 10:41:22 +0000

    OpenCL backend doc

--- src/backend/OpenCL/Backend.hh
+++ src/backend/OpenCL/Backend.hh
@@ -1,3 +1,14 @@
+/**
+ * @file Backend.hh
+ * @author Paulius Velesko (pvelesko@gmail.com)
+ * @brief OpenCL backend for HIPxx. HIPxxBackendOpenCL class definition with
+ * inheritance from HIPxxBackend. Subsequent virtual function overrides.
+ * @version 0.1
+ * @date 2021-08-19
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #ifndef HIPXX_BACKEND_OPENCL_H
 #define HIPXX_BACKEND_OPENCL_H
 
