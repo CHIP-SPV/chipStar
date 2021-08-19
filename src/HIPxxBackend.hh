6c4355557b603b0ad1f2722ff72d6f8ebb921d36
Paulius Velesko <paulius.velesko@intel.com>
2021-08-19 10:52:58 +0000

    HIPxxBackend.hh doc

--- src/HIPxxBackend.hh
+++ src/HIPxxBackend.hh
@@ -1,4 +1,16 @@
-
+/**
+ * @file HIPxxBackend.hh
+ * @author Paulius Velesko (pvelesko@gmail.com)
+ * @brief HIPxxBackend class definition. HIPxx backends are to inherit from this
+ * base class and override desired virtual functions. Overrides for this class
+ * are expected to be minimal with primary overrides being done on lower-level
+ * classes such as HIPxxContext consturctors, etc.
+ * @version 0.1
+ * @date 2021-08-19
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #ifndef HIPXX_BACKEND_H
 #define HIPXX_BACKEND_H
 
