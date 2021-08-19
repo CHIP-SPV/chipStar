27fcfefe1cb938eb39497b740bf14c35825c81fc
Paulius Velesko <paulius.velesko@intel.com>
2021-08-19 10:35:16 +0000

    HIPxxBindings.hh doc

--- src/HIPxxBindings.hh
+++ src/HIPxxBindings.hh
@@ -1,3 +1,18 @@
+/**
+ * @file HIPxxBindings.hh
+ * @author Paulius Velesko (pvelesko@gmail.com)
+ * @brief Implementations of the HIP API functions using the HIPxx interface
+ * providing basic functionality such hipMemcpy, host and device function
+ * registration, hipLaunchByPtr, etc.
+ * These functions operate on base HIPxx class pointers allowing for backend
+ * selection at runtime and backend-specific implementations are done by
+ * inheriting from base HIPxx classes and overriding virtual member functions.
+ * @version 0.1
+ * @date 2021-08-19
+ *
+ * @copyright Copyright (c) 2021
+ *
+ */
 #ifndef HIPXX_BINDINGS_H
 #define HIPXX_BINDINGS_H
 
