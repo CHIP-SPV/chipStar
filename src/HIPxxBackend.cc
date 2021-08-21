813b4ced43681ce1dde66bb59382294b0256c2c5
Paulius Velesko <paulius.velesko@intel.com>
2021-08-21 13:00:29 +0000

    Register Function

---
+++ src/HIPxxBackend.cc
@@ -0,0 +1,34 @@
+#include "HIPxxBackend.hh"
+
+bool HIPxxDevice::registerFunction(std::string* module_str,
+                                   const void* HostFunctionPtr,
+                                   const char* FunctionName) {
+  std::lock_guard<std::mutex> Lock(DeviceMutex);
+
+  // Get modules in binary representation
+  // These are extracted from the fat binary
+  std::vector<std::string*> modules_str = Backend->get_modules_str();
+
+  auto it = std::find(modules_str.begin(), modules_str.end(), module_str);
+  if (it == modules_str.end()) {
+    logError("Module PTR not FOUND: {%p}\n", (void*)module_str);
+    return false;
+  }
+
+  // Parse out HIPxxModule from binary representation
+  HIPxxModule* module = new HIPxxModule(module_str);
+
+  HostPtrToModuleStrMap.emplace(std::make_pair(HostFunctionPtr, module_str));
+  HostPtrToModuleStrMap.emplace(std::make_pair(HostFunctionPtr, module));
+  HostPtrToNameMap.emplace(std::make_pair(HostFunctionPtr, FunctionName));
+
+  // TODO Create & compile/createProgram a kernel
+  // Maybe this should be done in the Module constructor?
+  // HIPxxKernel kernel(HostFunctionPtr, FunctionName);
+  // module->add_kernel(kernel);
+
+  Modules.push_back(module);
+
+  // return (PrimaryContext->createProgramBuiltin(module, HostFunctionPtr, temp)
+  return true;
+}
--- src/HIPxxBindings.hh
+++ src/HIPxxBindings.hh
@@ -16,4 +16,34 @@
 #ifndef HIPXX_BINDINGS_H
 #define HIPXX_BINDINGS_H
 
+#include "HIPxxBackend.hh"
+#include "HIPxxDriver.hh"
+#include "temporary.hh"
+
+extern "C" void __hipRegisterFunction(void **data, const void *hostFunction,
+                                      char *deviceFunction,
+                                      const char *deviceName,
+                                      unsigned int threadLimit, void *tid,
+                                      void *bid, dim3 *blockDim, dim3 *gridDim,
+                                      int *wSize) {
+  Backend->initialize();
+  std::string *module_str = reinterpret_cast<std::string *>(data);
+
+  std::string devFunc = deviceFunction;
+  logDebug("RegisterFunction on module {}\n", (void *)module_str);
+
+  for (HIPxxDevice *dev : Backend->get_devices()) {
+    if (dev->registerFunction(module_str, hostFunction, deviceName)) {
+      logDebug("__hipRegisterFunction: kernel {} found\n", deviceName);
+    } else {
+      logCritical("__hipRegisterFunction can NOT find kernel: {} \n",
+                  deviceName);
+      std::abort();
+    }
+  }
+  // Put the function information into a temproary storage
+  // LZDriver::RegFunctions.push_back(
+  //    std::make_tuple(module, hostFunction, deviceName));
+}
+
 #endif
