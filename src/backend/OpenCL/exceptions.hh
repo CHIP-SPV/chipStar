a03cd6cfab866075804012fe2763a08984572880
Paulius Velesko <paulius.velesko@intel.com>
2021-08-24 10:05:32 +0000

    ifdef guards opencl exceptions

--- src/backend/OpenCL/exceptions.hh
+++ src/backend/OpenCL/exceptions.hh
@@ -1,3 +1,5 @@
+#ifndef OPENCL_EXCEPTIONS_H
+#define OPENCL_EXCEPTIONS_H
 class InvalidDeviceType : public std::invalid_argument {
   using std::invalid_argument::invalid_argument;
 };
@@ -5,3 +7,4 @@ class InvalidDeviceType : public std::invalid_argument {
 class InvalidPlatformOrDeviceNumber : public std::out_of_range {
   using std::out_of_range::out_of_range;
 };
+#endif
