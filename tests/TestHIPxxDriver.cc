34817909046c2f9889f28d0a5e68176853dabb56
Paulius Velesko <paulius.velesko@intel.com>
2021-08-26 08:25:26 +0000

    Backend initialization test

--- tests/TestHIPxxDriver.cc
+++ tests/TestHIPxxDriver.cc
@@ -3,13 +3,7 @@
 #include "HIPxxBackend.hh"
 #include "HIPxxDriver.hh"
 
-unsigned int Factorial(unsigned int number) {
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]") {
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
+TEST_CASE("Backend Initialization", "[HIPxxBackend]") {
+  initialize();
+  REQUIRE(Backend->hipxx_contexts.size() == 1);
 }
