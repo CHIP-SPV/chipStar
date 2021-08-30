3b067011d1174b246f3ba4819f3baeefbcfd4c3f
Paulius Velesko <paulius.velesko@intel.com>
2021-08-30 10:32:56 +0000

    Add HIPLZ RETURN() macros to temp

--- src/temporary.hh
+++ src/temporary.hh
@@ -6,4 +6,18 @@
 // #define logError(x, y) printf(x, y)
 // #define logCritical(x, y) printf(x, y)
 
+//    tls_LastError = err;
+#define RETURN(x)         \
+  do {                    \
+    hipError_t err = (x); \
+    return err;           \
+  } while (0)
+
+// tls_LastError = err;
+#define ERROR_IF(cond, err)                                                  \
+  if (cond) do {                                                             \
+      logError("Error {} at {}:{} code {}", err, __FILE__, __LINE__, #cond); \
+      return err;                                                            \
+  } while (0)
+
 #endif
