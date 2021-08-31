f247b0edbab2274a2e7f02711f6761a38852bf19
Paulius Velesko <paulius.velesko@intel.com>
2021-08-31 15:32:35 +0000

    Disabling Level0 Until I have access to HW

--- src/backend/Level0/Level0Backend.hh
+++ src/backend/Level0/Level0Backend.hh
@@ -1,6 +1,72 @@
 #ifndef HIPXX_BACKEND_LEVEL0_H
 #define HIPXX_BACKEND_LEVEL0_H
-
 class HIPxxBackendLevel0 : public HIPxxBackend {};
 
+#if 0
+#include "../../HIPxxBackend.hh"
+#include "ze_api.h"
+
+class HIPxxBackendLevel0 : public HIPxxBackend {
+ public:
+  virtual void initialize() override {
+    // Initialize the driver
+    ze_result_t status = zeInit(0);
+    // LZ_PROCESS_ERROR(status);
+    logDebug("INITIALIZE LEVEL-0 (via calling zeInit) {}\n", status);
+
+    // Initialize HipLZ device drivers and relevant devices
+    // LZDriver::InitDrivers(HipLZDrivers, ZE_DEVICE_TYPE_GPU);
+
+    // // Register fat binary modules
+    // for (std::string* module : LZDriver::FatBinModules) {
+    //   for (size_t driverId = 0; driverId < NumLZDrivers; ++driverId) {
+    //     LZDriver::HipLZDriverById(driverId).registerModule(module);
+    //   }
+    // }
+
+    // // Register functions
+    // for (auto fi : LZDriver::RegFunctions) {
+    //   std::string* module = std::get<0>(fi);
+    //   const void* hostFunction = std::get<1>(fi);
+    //   const char* deviceName = std::get<2>(fi);
+    //   for (size_t driverId = 0; driverId < NumLZDrivers; ++driverId) {
+    //     if (LZDriver::HipLZDriverById(driverId).registerFunction(
+    //             module, hostFunction, deviceName)) {
+    //       logDebug("__hipRegisterFunction: HipLZ kernel {} found\n",
+    //                deviceName);
+    //     } else {
+    //       logCritical("__hipRegisterFunction can NOT find HipLZ kernel: {}
+    //       \n",
+    //                   deviceName);
+    //       std::abort();
+    //     }
+    //   }
+    // }
+
+    // // Register globale variables
+    // for (auto vi : LZDriver::GlobalVars) {
+    //   std::string* module = std::get<0>(vi);
+    //   char* hostVar = std::get<1>(vi);
+    //   const char* deviceName = std::get<2>(vi);
+    //   int size = std::get<3>(vi);
+    //   std::string devName = deviceName;
+    //   for (size_t driverId = 0; driverId < NumLZDrivers; ++driverId) {
+    //     if (LZDriver::HipLZDriverById(driverId).registerVar(module, hostVar,
+    //                                                         deviceName,
+    //                                                         size)) {
+    //       logDebug("__hipRegisterVar: variable {} found\n", deviceName);
+    //     } else {
+    //       logError("__hipRegisterVar could not find: {}\n", deviceName);
+    //     }
+    //   }
+    // }
+  }
+
+  void uninitialize() override {
+    logTrace("HIPxxBackendLevel0 uninitializing");
+    logWarn("HIPxxBackendLevel0->uninitialize() not implemented");
+  }
+};
+
+#endif
 #endif
--- src/backend/backends.hh
+++ src/backend/backends.hh
@@ -1,7 +1,7 @@
 #ifndef HIPXX_BACKENDS_H
 #define HIPXX_BACKENDS_H
 
-#include "Level0/Backend.hh"
-#include "OpenCL/Backend.hh"
+#include "Level0/Level0Backend.hh"
+#include "OpenCL/OpenCLBackend.hh"
 
 #endif
