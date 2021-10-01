4943b3b8657f8be8da80f8e6c742bcbaceefd162
Paulius Velesko <paulius.velesko@intel.com>
2021-10-01 12:21:37 +0000

    test opencl and lz init

--- tests/TestHIPxxDriver.cc
+++ tests/TestHIPxxDriver.cc
@@ -3,9 +3,17 @@
 #include "HIPxxBackend.hh"
 #include "HIPxxDriver.hh"
 
-TEST_CASE("Backend init creates context, device, and a queue",
+TEST_CASE("OpenCL - Backend init creates context, device, and a queue",
           "[HIPxxBackend]") {
-  HIPxxInitialize();
+  HIPxxInitialize("OPENCL");
+  REQUIRE(Backend->hipxx_contexts.size() == 1);
+  REQUIRE(Backend->hipxx_devices.size() == 1);
+  REQUIRE(Backend->hipxx_queues.size() == 1);
+}
+
+TEST_CASE("Level0 - Backend init creates context, device, and a queue",
+          "[HIPxxBackend]") {
+  HIPxxInitialize("LEVEL0");
   REQUIRE(Backend->hipxx_contexts.size() == 1);
   REQUIRE(Backend->hipxx_devices.size() == 1);
   REQUIRE(Backend->hipxx_queues.size() == 1);
