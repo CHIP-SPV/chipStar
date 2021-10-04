e4fc54650e3e7a2eae0930a59d2fe2fce5937738
Paulius Velesko <paulius.velesko@intel.com>
2021-10-04 10:21:18 +0000

    hipGetLastError, hipPeekAtLastError, hipGetErrorName, hipGetErrorString

--- src/HIPxxBindings.cc
+++ src/HIPxxBindings.cc
@@ -299,6 +299,154 @@ hipError_t hipRuntimeGetVersion(int *runtimeVersion) {
   } else
     RETURN(hipErrorInvalidValue);
 }
+
+hipError_t hipGetLastError(void) {
+  HIPxxInitialize();
+
+  hipError_t temp = Backend->tls_last_error;
+  Backend->tls_last_error = hipSuccess;
+  return temp;
+}
+
+hipError_t hipPeekAtLastError(void) {
+  HIPxxInitialize();
+
+  return Backend->tls_last_error;
+}
+
+const char *hipGetErrorName(hipError_t hip_error) {
+  switch (hip_error) {
+    case hipSuccess:
+      return "hipSuccess";
+    case hipErrorOutOfMemory:
+      return "hipErrorOutOfMemory";
+    case hipErrorNotInitialized:
+      return "hipErrorNotInitialized";
+    case hipErrorDeinitialized:
+      return "hipErrorDeinitialized";
+    case hipErrorProfilerDisabled:
+      return "hipErrorProfilerDisabled";
+    case hipErrorProfilerNotInitialized:
+      return "hipErrorProfilerNotInitialized";
+    case hipErrorProfilerAlreadyStarted:
+      return "hipErrorProfilerAlreadyStarted";
+    case hipErrorProfilerAlreadyStopped:
+      return "hipErrorProfilerAlreadyStopped";
+    case hipErrorInvalidImage:
+      return "hipErrorInvalidImage";
+    case hipErrorInvalidContext:
+      return "hipErrorInvalidContext";
+    case hipErrorContextAlreadyCurrent:
+      return "hipErrorContextAlreadyCurrent";
+    case hipErrorMapFailed:
+      return "hipErrorMapFailed";
+    case hipErrorUnmapFailed:
+      return "hipErrorUnmapFailed";
+    case hipErrorArrayIsMapped:
+      return "hipErrorArrayIsMapped";
+    case hipErrorAlreadyMapped:
+      return "hipErrorAlreadyMapped";
+    case hipErrorNoBinaryForGpu:
+      return "hipErrorNoBinaryForGpu";
+    case hipErrorAlreadyAcquired:
+      return "hipErrorAlreadyAcquired";
+    case hipErrorNotMapped:
+      return "hipErrorNotMapped";
+    case hipErrorNotMappedAsArray:
+      return "hipErrorNotMappedAsArray";
+    case hipErrorNotMappedAsPointer:
+      return "hipErrorNotMappedAsPointer";
+    case hipErrorECCNotCorrectable:
+      return "hipErrorECCNotCorrectable";
+    case hipErrorUnsupportedLimit:
+      return "hipErrorUnsupportedLimit";
+    case hipErrorContextAlreadyInUse:
+      return "hipErrorContextAlreadyInUse";
+    case hipErrorPeerAccessUnsupported:
+      return "hipErrorPeerAccessUnsupported";
+    case hipErrorInvalidKernelFile:
+      return "hipErrorInvalidKernelFile";
+    case hipErrorInvalidGraphicsContext:
+      return "hipErrorInvalidGraphicsContext";
+    case hipErrorInvalidSource:
+      return "hipErrorInvalidSource";
+    case hipErrorFileNotFound:
+      return "hipErrorFileNotFound";
+    case hipErrorSharedObjectSymbolNotFound:
+      return "hipErrorSharedObjectSymbolNotFound";
+    case hipErrorSharedObjectInitFailed:
+      return "hipErrorSharedObjectInitFailed";
+    case hipErrorOperatingSystem:
+      return "hipErrorOperatingSystem";
+    case hipErrorSetOnActiveProcess:
+      return "hipErrorSetOnActiveProcess";
+    case hipErrorInvalidHandle:
+      return "hipErrorInvalidHandle";
+    case hipErrorNotFound:
+      return "hipErrorNotFound";
+    case hipErrorIllegalAddress:
+      return "hipErrorIllegalAddress";
+    case hipErrorInvalidSymbol:
+      return "hipErrorInvalidSymbol";
+    case hipErrorMissingConfiguration:
+      return "hipErrorMissingConfiguration";
+    case hipErrorMemoryAllocation:
+      return "hipErrorMemoryAllocation";
+    case hipErrorInitializationError:
+      return "hipErrorInitializationError";
+    case hipErrorLaunchFailure:
+      return "hipErrorLaunchFailure";
+    case hipErrorPriorLaunchFailure:
+      return "hipErrorPriorLaunchFailure";
+    case hipErrorLaunchTimeOut:
+      return "hipErrorLaunchTimeOut";
+    case hipErrorLaunchOutOfResources:
+      return "hipErrorLaunchOutOfResources";
+    case hipErrorInvalidDeviceFunction:
+      return "hipErrorInvalidDeviceFunction";
+    case hipErrorInvalidConfiguration:
+      return "hipErrorInvalidConfiguration";
+    case hipErrorInvalidDevice:
+      return "hipErrorInvalidDevice";
+    case hipErrorInvalidValue:
+      return "hipErrorInvalidValue";
+    case hipErrorInvalidDevicePointer:
+      return "hipErrorInvalidDevicePointer";
+    case hipErrorInvalidMemcpyDirection:
+      return "hipErrorInvalidMemcpyDirection";
+    case hipErrorUnknown:
+      return "hipErrorUnknown";
+    case hipErrorInvalidResourceHandle:
+      return "hipErrorInvalidResourceHandle";
+    case hipErrorNotReady:
+      return "hipErrorNotReady";
+    case hipErrorNoDevice:
+      return "hipErrorNoDevice";
+    case hipErrorPeerAccessAlreadyEnabled:
+      return "hipErrorPeerAccessAlreadyEnabled";
+    case hipErrorNotSupported:
+      return "hipErrorNotSupported";
+    case hipErrorPeerAccessNotEnabled:
+      return "hipErrorPeerAccessNotEnabled";
+    case hipErrorRuntimeMemory:
+      return "hipErrorRuntimeMemory";
+    case hipErrorRuntimeOther:
+      return "hipErrorRuntimeOther";
+    case hipErrorHostMemoryAlreadyRegistered:
+      return "hipErrorHostMemoryAlreadyRegistered";
+    case hipErrorHostMemoryNotRegistered:
+      return "hipErrorHostMemoryNotRegistered";
+    case hipErrorTbd:
+      return "hipErrorTbd";
+    default:
+      return "hipErrorUnknown";
+  }
+}
+
+const char *hipGetErrorString(hipError_t hipError) {
+  return hipGetErrorName(hipError);
+}
+
 //*****************************************************************************
 //*****************************************************************************
 //*****************************************************************************
@@ -422,16 +570,6 @@ hipError_t hipEventDestroy(hipEvent_t event) {
   // ERROR_IF((event == nullptr), hipErrorInvalidValue);
 
   // delete event;
-  RETURN(hipSuccess);
-}
-
-hipError_t hipGetLastError(void) {
-  // logWarn("hipGetLastError not yet implemented");
-  // HIPLZ_INIT();
-
-  // hipError_t temp = tls_LastError;
-  // tls_LastError = hipSuccess;
-  // return temp;
   RETURN(hipSuccess);
 }
 
