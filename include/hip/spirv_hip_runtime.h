bce301fb0b5492f38cfe62c332507194430ed373
Paulius Velesko <paulius.velesko@intel.com>
2021-10-25 14:15:49 +0000

    rename hip_runtime.h to spirv_hip_runtime.h

---
+++ include/hip/spirv_hip_runtime.h
@@ -0,0 +1,20 @@
+
+#ifndef SPIRV_HIP_RUNTIME_H
+#define SPIRV_HIP_RUNTIME_H
+
+#ifndef __HIP_PLATFORM_SPIRV__
+#define __HIP_PLATFORM_SPIRV__
+#endif
+
+#include <cmath>
+#include <cstdint>
+
+#include <hip/hip_runtime_api.h>
+
+#include <hip/spirv_hip.hh>
+
+#include <hip/spirv_hip_vector_types.h>
+
+#include <hip/spirv_hip_fp16.h>
+
+#endif
