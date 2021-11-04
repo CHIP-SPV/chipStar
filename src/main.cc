f0b7af3acf905852c69943feeaa1f70662218934
Paulius Velesko <paulius.velesko@intel.com>
2021-11-04 14:51:46 +0000

    update main.cc

--- src/main.cc
+++ src/main.cc
@@ -8,4 +8,8 @@ int main() {
   hipDevice_t dev;
   auto err = hipGetDevice(&dev);
   if (err != hipSuccess) std::cout << hipGetErrorName(err) << std::endl;
+
+  hipStream_t stream;
+  err = hipStreamCreateWithPriority(&stream, 0, 0);
+  std::cout << hipGetErrorName(err) << std::endl;
 }
