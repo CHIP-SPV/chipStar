86cb5a716ed9fc61f9405c89b5adba53ebf8b497
Paulius Velesko <pvelesko@pglc.io>
2022-01-21 10:27:35 +0000

    lint exceptions.hh

--- src/backend/OpenCL/exceptions.hh
+++ src/backend/OpenCL/exceptions.hh
@@ -1,5 +1,8 @@
 #ifndef OPENCL_EXCEPTIONS_H
 #define OPENCL_EXCEPTIONS_H
+
+#include <stdexcept>
+
 class InvalidDeviceType : public std::invalid_argument {
   using std::invalid_argument::invalid_argument;
 };
