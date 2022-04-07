parmance/chip-spv

printf: Fix dynamic string args.
Trusts that the frontend emits the literal strings to
the global address space. If we emitted them to the constant
space, there would be illegal ascasts between constant and
generic.

dynamic_str_args is a test suite that passes with PoCL/CPU
but fails with LZ. We need a conditional XFAIL checking at
runtime if we want to add it to the test suite.

llvm_passes/HipPrintf.h
@@ -1,19 +1,40 @@
+//===- HipPrintf.cppp -----------------------------------------------===//
+//
+// Part of the CHIP-SPV Project, under the Apache License v2.0 with LLVM
+// Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// LLVM IR pass to convert calls to the CUDA/HIP printf() to OpenCL/SPIR-V
+// compatible ones.
+//
+// (c) 2021-2022 Pekka Jääskeläinen / Parmance for Argonne National Laboratory
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_PASSES_HIP_PRINTF_H
 #define LLVM_PASSES_HIP_PRINTF_H
 
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/PassManager.h"
 
 using namespace llvm;
 
-#define SPIRV_OPENCL_PRINTF_FMT_ARG_AS 2
-
-#if LLVM_VERSION_MAJOR > 11
 class HipPrintfToOpenCLPrintfPass
     : public PassInfoMixin<HipPrintfToOpenCLPrintfPass> {
 public:
-  PreservedAnalyses run(Function &M, FunctionAnalysisManager &AM);
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
   static bool isRequired() { return true; }
+
+private:
+  Constant *getOrCreateStrLiteralArg(
+      const std::string& Str, llvm::IRBuilder<>& B);
+  Function *getOrCreatePrintStringF();
+  Value* cloneStrArgToConstantAS(
+    Value *StrArg, llvm::IRBuilder<>& B, bool *IsEmpty);
+
+  std::map<std::string, Constant*> LiteralArgs;
+  Module *M;
 };
-#endif
 
 #endif

CHIP-SPV/chipStar

llvm_passes/HipPrintf.h
@@ -1,19 +1,42 @@
+//===- HipPrintf.cppp -----------------------------------------------===//
+//
+// Part of the CHIP-SPV Project, under the Apache License v2.0 with LLVM
+// Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// LLVM IR pass to convert calls to the CUDA/HIP printf() to OpenCL/SPIR-V
+// compatible ones.
+//
+// (c) 2021-2022 Pekka Jääskeläinen / Parmance for Argonne National Laboratory
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_PASSES_HIP_PRINTF_H
 #define LLVM_PASSES_HIP_PRINTF_H
 
+#include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/PassManager.h"
+
+#include <map>
 
 using namespace llvm;
 
-#define SPIRV_OPENCL_PRINTF_FMT_ARG_AS 2
-
-#if LLVM_VERSION_MAJOR > 11
 class HipPrintfToOpenCLPrintfPass
     : public PassInfoMixin<HipPrintfToOpenCLPrintfPass> {
 public:
-  PreservedAnalyses run(Function &M, FunctionAnalysisManager &AM);
+  PreservedAnalyses run(Module &M, ModuleAnalysisManager &AM);
   static bool isRequired() { return true; }
+
+private:
+  Constant *getOrCreateStrLiteralArg(
+      const std::string& Str, llvm::IRBuilder<>& B);
+  Function *getOrCreatePrintStringF();
+  Value* cloneStrArgToConstantAS(
+    Value *StrArg, llvm::IRBuilder<>& B, bool *IsEmpty);
+
+  std::map<std::string, Constant*> LiteralArgs_;
+  Module *M_;
 };
-#endif
 
 #endif