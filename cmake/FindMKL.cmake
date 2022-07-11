0796d378f8a76bc6af716d9ea2e53c0f9982e3c8
Michal Babej <michal.babej@parmance.com>
2022-07-11 17:58:10 +0300

    move MKL & ICPX search into cmake/mkl_and_icpx.cmake

--- cmake/FindMKL.cmake
+++
@@ -1,7 +0,0 @@
-# Build LLVM-PASSES 
-set(MKL_DIR "" CACHE PATH "Path to MKL install part of oneapi")
-if(MKL_DIR STREQUAL "") 
-  message(FATAL_ERROR "icpx was found, but -DMKL_DIR option was not passed to CMake.")
-endif()
-
-# TODO: find MKL based on paths relative to where icpx lives
