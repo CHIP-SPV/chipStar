66ae81e5431c9ce9328a0d3421efabe67f1454ce
Paulius Velesko <pvelesko@pglc.io>
2022-07-20 17:06:40 +0000

    bloaf16 math

---
+++ include/hip/devicelib/bfloat16/bfloat162_math.hh
@@ -0,0 +1,22 @@
+#ifndef HIP_INCLUDE_DEVICELIB_BFLOAT162_MATH_H
+#define HIP_INCLUDE_DEVICELIB_BFLOAT162_MATH_H
+
+#include <hip/devicelib/macros.hh>
+
+// __device__​ __nv_bfloat162 	h2ceil ( const __nv_bfloat162 h )
+// __device__​ __nv_bfloat162 	h2cos ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2exp ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2exp10 ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2exp2 ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2floor ( const __nv_bfloat162 h )
+// __device__​ __nv_bfloat162 	h2log ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2log10 ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2log2 ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2rcp ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2rint ( const __nv_bfloat162 h )
+// __device__​ __nv_bfloat162 	h2rsqrt ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2sin ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2sqrt ( const __nv_bfloat162 a )
+// __device__​ __nv_bfloat162 	h2trunc ( const __nv_bfloat162 h )
+
+#endif // include guard
