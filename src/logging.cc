781ec3b802c8d5de17e96a23e8b9bc1c8e4ef467
Michal Babej <michal.babej@parmance.com>
2022-08-01 13:38:48 +0300

    logging: simplify & fix a bug with loglevel trace
    with CHIP_LOGLEVEL=trace, the actual loglevel used was debug
    
    default to DEBUG level for debug builds, WARN for release builds

--- src/logging.cc
+++ src/logging.cc
@@ -1,4 +1,5 @@
 #include "logging.hh"
+
 #include <iostream>
 #include <mutex>
 
@@ -8,15 +9,21 @@ void setupSpdlog() { std::call_once(SpdlogWasSetup, &_setupSpdlog); }
 
 void _setupSpdlog() {
   spdlog::set_default_logger(spdlog::stderr_color_mt("CHIP"));
-  spdlog::set_level(spdlog::level::debug);
   spdlog::set_pattern("%n %^%l%$ [TID %t] [%E.%F] : %v");
 
-  spdlog::level::level_enum SpdLogLevel;
+  spdlog::level::level_enum SpdLogLevel =
+#if (SPDLOG_ACTIVE_LEVEL == SPDLOG_LEVEL_TRACE)
+      spdlog::level::debug;
+#else
+      spdlog::level::warn;
+#endif
 
   const char *LogLevel = getenv("CHIP_LOGLEVEL");
   if (LogLevel) {
     // std::cout << "CHIP_LOGLEVEL=" << loglevel << "\n";
     std::string Level(LogLevel);
+    if (Level == "trace")
+      SpdLogLevel = spdlog::level::trace;
     if (Level == "debug")
       SpdLogLevel = spdlog::level::debug;
     if (Level == "info")
@@ -29,9 +36,6 @@ void _setupSpdlog() {
       SpdLogLevel = spdlog::level::critical;
     if (Level == "off")
       SpdLogLevel = spdlog::level::off;
-  } else {
-    // std::cout << "CHIP_LOGLEVEL was not set. Default to trace\n";
-    SpdLogLevel = spdlog::level::trace;
   }
 
   spdlog::set_level(SpdLogLevel);
