c5fc56b4ccbb9527807f8e49c270524b39f0fcc5
Michal Babej <michal.babej@parmance.com>
2022-08-03 17:07:48 +0300

    fix commit "math library: add integer max/min/abs" - move declarations to int_math.hh

--- include/hip/devicelib/integer/int_math.hh
+++ include/hip/devicelib/integer/int_math.hh
@@ -3,60 +3,125 @@
 
 #include <hip/devicelib/macros.hh>
 
-// __device__ int abs ( int  a )
-// __device__ long int labs ( long int  a )
+#if defined(__HIP_DEVICE_COMPILE__)
+extern "C" {
+NON_OVLD int GEN_NAME2(max, i)(int a, int b);
+NON_OVLD unsigned GEN_NAME2(max, u)(unsigned int a, unsigned int b);
+NON_OVLD long int GEN_NAME2(max, l)(long int a, long int b);
+NON_OVLD unsigned long int GEN_NAME2(max, ul)(unsigned long int a,
+                                              unsigned long int b);
+
+NON_OVLD int GEN_NAME2(min, i)(int a, int b);
+NON_OVLD unsigned GEN_NAME2(min, u)(unsigned int a, unsigned int b);
+NON_OVLD long int GEN_NAME2(min, l)(long int a, long int b);
+NON_OVLD unsigned long int GEN_NAME2(min, ul)(unsigned long int a,
+                                              unsigned long int b);
+
+NON_OVLD int GEN_NAME2(abs, i)(int a);
+NON_OVLD long int GEN_NAME2(abs, l)(long int a);
+}
+
+EXPORT OVLD int abs(int a) { return GEN_NAME2(abs, i)(a); }
+EXPORT OVLD long int labs(long int a) { return GEN_NAME2(abs, l)(a); }
+
+EXPORT OVLD unsigned long int max(const unsigned long int a,
+                                  const unsigned long int b) {
+  return GEN_NAME2(max, ul)(a, b);
+}
+EXPORT OVLD unsigned long int max(const unsigned long int a, const long int b) {
+  return (b < 0) ? a : GEN_NAME2(max, ul)(a, (unsigned long)b);
+}
+EXPORT OVLD unsigned long int max(const long int a, const unsigned long int b) {
+  return max(b, a);
+}
+EXPORT OVLD long int max(const long int a, const long int b) {
+  return GEN_NAME2(max, l)(a, b);
+}
+
+EXPORT OVLD unsigned int max(const unsigned int a, const unsigned int b) {
+  return GEN_NAME2(max, u)(a, b);
+}
+EXPORT OVLD int max(const int a, const int b) {
+  return GEN_NAME2(max, i)(a, b);
+}
+EXPORT OVLD unsigned int max(const unsigned int a, const int b) {
+  return (b < 0) ? a : GEN_NAME2(max, u)(a, (unsigned)b);
+}
+EXPORT OVLD unsigned int max(const int a, const unsigned int b) {
+  return max(b, a);
+}
+
+EXPORT OVLD unsigned long int min(const unsigned long int a,
+                                  const unsigned long int b) {
+  return GEN_NAME2(min, ul)(a, b);
+}
+
+EXPORT OVLD unsigned long int min(const unsigned long int a, const long int b) {
+  return (b < 0) ? a : GEN_NAME2(min, ul)(a, (unsigned long)b);
+}
+EXPORT OVLD unsigned long int min(const long int a, const unsigned long int b) {
+  return min(b, a);
+}
+EXPORT OVLD long int min(const long int a, const long int b) {
+  return GEN_NAME2(min, l)(a, b);
+}
+
+EXPORT OVLD unsigned int min(const unsigned int a, const unsigned int b) {
+  return GEN_NAME2(min, u)(a, b);
+}
+EXPORT OVLD int min(const int a, const int b) {
+  return GEN_NAME2(min, i)(a, b);
+}
+EXPORT OVLD unsigned int min(const unsigned int a, const int b) {
+  return (b < 0) ? a : GEN_NAME2(min, u)(a, (unsigned)b);
+}
+EXPORT OVLD unsigned int min(const int a, const unsigned int b) {
+  return min(b, a);
+}
+
+EXPORT OVLD unsigned int umax(const unsigned int a, const unsigned int b) {
+  return GEN_NAME2(max, u)(a, b);
+}
+EXPORT OVLD unsigned int umin(const unsigned int a, const unsigned int b) {
+  return GEN_NAME2(min, u)(a, b);
+}
+
+#else
+EXPORT OVLD int abs(int a);
+EXPORT OVLD long int labs(long int a);
+EXPORT OVLD unsigned long int max(const unsigned long int a, const long int b);
+EXPORT OVLD unsigned long int max(const long int a, const unsigned long int b);
+EXPORT OVLD unsigned long int max(const unsigned long int a,
+                                  const unsigned long int b);
+EXPORT OVLD long int max(const long int a, const long int b);
+EXPORT OVLD unsigned int max(const unsigned int a, const int b);
+EXPORT OVLD unsigned int max(const int a, const unsigned int b);
+EXPORT OVLD unsigned int max(const unsigned int a, const unsigned int b);
+EXPORT OVLD int max(const int a, const int b);
+EXPORT OVLD unsigned long int min(const unsigned long int a, const long int b);
+EXPORT OVLD unsigned long int min(const long int a, const unsigned long int b);
+EXPORT OVLD unsigned long int min(const unsigned long int a,
+                                  const unsigned long int b);
+EXPORT OVLD long int min(const long int a, const long int b);
+EXPORT OVLD unsigned int min(const unsigned int a, const int b);
+EXPORT OVLD unsigned int min(const int a, const unsigned int b);
+EXPORT OVLD unsigned int min(const unsigned int a, const unsigned int b);
+EXPORT OVLD int min(const int a, const int b);
+
+EXPORT OVLD unsigned int umax(const unsigned int a, const unsigned int b);
+EXPORT OVLD unsigned int umin(const unsigned int a, const unsigned int b);
+#endif
+
+
 // __device__ long long int 	llabs ( long long int a )
 // __device__ long long int 	llmax ( const long long int a, const long long
 // int b )
 // __device__ long long int 	llmin ( const long long int a, const long long
 // int b )
-// __device__ unsigned long long int 	max ( const unsigned long long int a,
-// const long long int b )
-// __device__ unsigned long long int 	max ( const long long int a, const
-// unsigned long long int b )
-// __device__ unsigned long long int 	max ( const unsigned long long int a,
-// const unsigned long long int b )
-// __device__ long long int 	max ( const long long int a, const long long int
-// b )
-// __device__ unsigned long int 	max ( const unsigned long int a, const
-// long int  b )
-// __device__ unsigned long int 	max ( const long int  a, const unsigned
-// long int b )
-// __device__ unsigned long int 	max ( const unsigned long int a, const
-// unsigned long int b )
-// __device__ long int max ( const long int  a, const long int  b )
-// __device__ unsigned int 	max ( const unsigned int  a, const int  b )
-// __device__ unsigned int 	max ( const int  a, const unsigned int  b )
-// __device__ unsigned int 	max ( const unsigned int  a, const unsigned int
-// b )
-// __device__ int max ( const int  a, const int  b )
-// __device__ unsigned long long int 	min ( const unsigned long long int a,
-// const long long int b )
-// __device__ unsigned long long int 	min ( const long long int a, const
-// unsigned long long int b )
-// __device__ unsigned long long int 	min ( const unsigned long long int a,
-// const unsigned long long int b )
-// __device__ long long int 	min ( const long long int a, const long long int
-// b )
-// __device__ unsigned long int 	min ( const unsigned long int a, const
-// long int  b )
-// __device__ unsigned long int 	min ( const long int  a, const unsigned
-// long int b )
-// __device__ unsigned long int 	min ( const unsigned long int a, const
-// unsigned long int b )
-// __device__ long int min ( const long int  a, const long int  b )
-// __device__ unsigned int 	min ( const unsigned int  a, const int  b )
-// __device__ unsigned int 	min ( const int  a, const unsigned int  b )
-// __device__ unsigned int 	min ( const unsigned int  a, const unsigned int
-// b )
-// __device__ int min ( const int  a, const int  b )
+
 // __device__ unsigned long long int 	ullmax ( const unsigned long long int a,
 // const unsigned long long int b )
 // __device__ unsigned long long int 	ullmin ( const unsigned long long int a,
 // const unsigned long long int b )
-// __device__ unsigned int 	umax ( const unsigned int  a, const unsigned int
-// b )
-// __device__ unsigned int 	umin ( const unsigned int  a, const unsigned int
-// b )
 
 #endif // include guard
