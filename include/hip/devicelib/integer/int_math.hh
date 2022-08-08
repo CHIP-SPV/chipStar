f6fe3babe64b04821ad866d5eec4846b52fcfd1d
Michal Babej <michal.babej@parmance.com>
2022-08-08 16:48:59 +0300

    devicelib/integer/int_math.hh: fix problem with long long
    "long long int" and types (and functions using them) apparently
    don't map to SPIR-V's OpenCL extended instruction set. As a result,
    tests fail because there are unresolved "extern __device__ long long .."
    functions in the SPIR-V.
    
    Replace "extern __device__ long long ..." with in-place functions
    that cast arguments to long.

--- include/hip/devicelib/integer/int_math.hh
+++ include/hip/devicelib/integer/int_math.hh
@@ -10,22 +10,18 @@ extern __device__ unsigned max(unsigned int a, unsigned int b);
 extern __device__ long int max(long int a, long int b);
 extern __device__ unsigned long int max(unsigned long int a,
                                         unsigned long int b);
-extern __device__ unsigned long long int max(unsigned long long int a,
-                                             unsigned long long int b);
 extern __device__ int min(int a, int b);
 extern __device__ unsigned min(unsigned int a, unsigned int b);
 extern __device__ long int min(long int a, long int b);
 extern __device__ unsigned long int min(unsigned long int a,
                                         unsigned long int b);
-extern __device__ unsigned long int min(unsigned long long int a,
-                                        unsigned long long int b);
 
 extern __device__ int abs(int a);
 extern __device__ long int abs(long int a);
-extern __device__ long long int abs(long long int a);
 
 __device__ long int labs(long int a) { return abs(a); }
-__device__ long long llabs(long long int a) { return abs(a); }
+__device__ long long int abs(long long int a) { return abs((long int)a); }
+__device__ long long int llabs(long long int a) { return abs((long int)a); }
 
 __device__ unsigned long int max(const unsigned long int a, const long int b) {
   return (b < 0) ? a : max(a, (unsigned long)b);
@@ -63,13 +59,32 @@ __device__ unsigned int umin(const unsigned int a, const unsigned int b) {
   return min(a, b);
 }
 
+__device__ unsigned long long int max(unsigned long long int a,
+                                      unsigned long long int b) {
+  return max((unsigned long)a, (unsigned long)b);
+}
+
+__device__ unsigned long long int min(unsigned long long int a,
+                                      unsigned long long int b) {
+  return min((unsigned long)a, (unsigned long)b);
+}
+
 __device__ unsigned long long int ullmax(const unsigned long long int a,
                                          const unsigned long long int b) {
-  return max(a, b);
-};
+  return max((unsigned long)a, (unsigned long)b);
+}
+
 __device__ unsigned long long int ullmin(const unsigned long long int a,
                                          const unsigned long long int b) {
-  return min(a, b);
+  return min((unsigned long)a, (unsigned long)b);
+}
+
+__device__ long long int llmax(const long long int a, const long long int b) {
+  return max((long)a, (long)b);
+}
+
+__device__ long long int llmin(const long long int a, const long long int b) {
+  return min((long)a, (long)b);
 }
 }
 
@@ -103,6 +118,11 @@ extern __device__ unsigned int min(const int a, const unsigned int b);
 extern __device__ unsigned int min(const unsigned int a, const unsigned int b);
 extern __device__ int min(const int a, const int b);
 
+extern __device__ long long int llmax(const long long int a,
+                                      const long long int b);
+extern __device__ long long int llmin(const long long int a,
+                                      const long long int b);
+
 extern __device__ unsigned int umax(const unsigned int a, const unsigned int b);
 extern __device__ unsigned int umin(const unsigned int a, const unsigned int b);
 extern __device__ unsigned long long int ullmax(const unsigned long long int a,
@@ -112,14 +132,5 @@ extern __device__ unsigned long long int ullmin(const unsigned long long int a,
 }
 
 #endif
-
-// __device__ long long int 	llmax ( const long long int a, const long long
-// int b )
-// __device__ long long int 	llmin ( const long long int a, const long long
-// int b )
-// __device__ unsigned long long int 	ullmax ( const unsigned long long int a,
-// const unsigned long long int b )
-// __device__ unsigned long long int 	ullmin ( const unsigned long long int a,
-// const unsigned long long int b )
 
 #endif // include guard
