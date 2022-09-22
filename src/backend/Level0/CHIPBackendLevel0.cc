54624d523fce8e9bc6a4c1c5e76af660b39f7692
Pekka Jääskeläinen <pekka.jaaskelainen@intel.com>
2022-09-22 15:24:53 +0300

    Fix OpenCL emulation of host-mapped memory.
    The problem was visible with the hipHostRegister test which
    allocates a host buffer with malloc() and then calls hipHostRegister()
    to register it and hipHostGetDevicePointer() to get the device-side
    shadow buffer.  The copy was erroneously skipped in this case
    since there was a false assumption that with the OpenCL BE we
    allocate everything via clSVMAlloc().  The skipping happens in
    CHIPBackend.cc:1610.
    
    There should be a better logic that checks if the buffer was originally
    allocated via SVM in this case instead so we can skip the shadow buf
    synch safely.
    
    Also renames
    
     virtual bool isAllocatedPtrUSM(void *Ptr) = 0;
     to
     virtual bool isAllocatedPtrMappedToVM(void *Ptr) = 0;
    
    to avoid confusion.
    
    USM = Unified Shared Memory, which is the LevelZero
    memory model and the Intel OpenCL extension:
    https://registry.khronos.org/OpenCL/extensions/intel/cl_intel_unified_shared_memory.html

--- src/backend/Level0/CHIPBackendLevel0.cc
+++ src/backend/Level0/CHIPBackendLevel0.cc
@@ -1360,7 +1360,7 @@ void CHIPBackendLevel0::initializeImpl(std::string CHIPPlatformStr,
   ze_result_t Status;
   Status = zeInit(0);
   if (Status != ZE_RESULT_SUCCESS) {
-    logError("zeInit failed ");
+    logError("zeInit failed with code {}", Status);
     std::abort();
   }
 
