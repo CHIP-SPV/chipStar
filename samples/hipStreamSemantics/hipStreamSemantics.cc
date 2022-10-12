0022dadf549424491d5a2363f4138c3691c91c60
Paulius Velesko <pvelesko@pglc.io>
2022-10-12 12:34:53 +0000

    format

--- samples/hipStreamSemantics/hipStreamSemantics.cc
+++ samples/hipStreamSemantics/hipStreamSemantics.cc
@@ -39,222 +39,233 @@ __global__ void addOne(int *__restrict A) {
   A[i] = A[i] + 1;
 }
 
-void callback_sleep2(hipStream_t stream, hipError_t status, void* user_data) {
-    int *data = (int*)user_data;
-    printf("callback_sleep2: Going to sleep for 2sec\n");
-    sleep(2);
-    *data = 2;
-    printf("callback_sleep2: Exiting now\n");
+void callback_sleep2(hipStream_t stream, hipError_t status, void *user_data) {
+  int *data = (int *)user_data;
+  printf("callback_sleep2: Going to sleep for 2sec\n");
+  sleep(2);
+  *data = 2;
+  printf("callback_sleep2: Exiting now\n");
 }
 
-void callback_sleep10(hipStream_t stream, hipError_t status, void* user_data) {
-    int *data = (int*)user_data;
-    printf("callback_sleep10: Going to sleep for 10sec\n");
-    sleep(10);
-    *data = 2;
-    printf("callback_sleep10: Exiting now\n");
+void callback_sleep10(hipStream_t stream, hipError_t status, void *user_data) {
+  int *data = (int *)user_data;
+  printf("callback_sleep10: Going to sleep for 10sec\n");
+  sleep(10);
+  *data = 2;
+  printf("callback_sleep10: Exiting now\n");
 }
 
 /*
  * Intent : Verify hipStreamQuery returns right queue status
  */
 bool TestStreamSemantics_1() {
-    printf("------------------------------------------------------------------\n");
-    int* stream2_shared_data = nullptr;
-    hipError_t status;
-    hipStream_t stream;
-    CHECK(hipStreamCreate(&stream));
-    stream2_shared_data = (int*)malloc(sizeof(int));
-    CHECK(hipStreamAddCallback(stream, callback_sleep10, stream2_shared_data, 0));
-    status = hipStreamQuery(stream);
-    bool testStatus = true;
-    printf("%s(stream query) : ", __FUNCTION__);
-    if (status != hipErrorNotReady) {
-        printf("%s%s%s\n","\033[0;31m", "Failed", "\033[0m");
-        testStatus = false;
-        //printf("Failed, queue status is %s but expected is hipErrorNotReady\n", hipGetErrorName(status));
-    } else {
-        printf("Passed\n");
-    }
+  printf(
+      "------------------------------------------------------------------\n");
+  int *stream2_shared_data = nullptr;
+  hipError_t status;
+  hipStream_t stream;
+  CHECK(hipStreamCreate(&stream));
+  stream2_shared_data = (int *)malloc(sizeof(int));
+  CHECK(hipStreamAddCallback(stream, callback_sleep10, stream2_shared_data, 0));
+  status = hipStreamQuery(stream);
+  bool testStatus = true;
+  printf("%s(stream query) : ", __FUNCTION__);
+  if (status != hipErrorNotReady) {
+    printf("%s%s%s\n", "\033[0;31m", "Failed", "\033[0m");
+    testStatus = false;
+    // printf("Failed, queue status is %s but expected is hipErrorNotReady\n",
+    // hipGetErrorName(status));
+  } else {
+    printf("Passed\n");
+  }
 
-    // Wait for all tasks to be finished
-    CHECK(hipDeviceSynchronize());
-    // Clean-up
-    CHECK(hipStreamDestroy(stream));
-    free(stream2_shared_data);
+  // Wait for all tasks to be finished
+  CHECK(hipDeviceSynchronize());
+  // Clean-up
+  CHECK(hipStreamDestroy(stream));
+  free(stream2_shared_data);
 
-    return testStatus;
+  return testStatus;
 }
 
 /*
  * Intent : Verify non-blocking stream is indeed non-blocking
  */
 bool TestStreamSemantics_2() {
-    printf("------------------------------------------------------------------\n");
-    // Init
-    hipStream_t stream_non_blocking;
-    CHECK(hipStreamCreateWithFlags(&stream_non_blocking, hipStreamNonBlocking));
-    int* stream_shared_data = nullptr;
-    stream_shared_data = (int*)malloc(sizeof(int));
-    *stream_shared_data = 1;
+  printf(
+      "------------------------------------------------------------------\n");
+  // Init
+  hipStream_t stream_non_blocking;
+  CHECK(hipStreamCreateWithFlags(&stream_non_blocking, hipStreamNonBlocking));
+  int *stream_shared_data = nullptr;
+  stream_shared_data = (int *)malloc(sizeof(int));
+  *stream_shared_data = 1;
 
-    int *host_ptr = nullptr;
-    int *dev_ptr = nullptr;
-    size_t size = sizeof(int);
-    CHECK(hipMalloc(&dev_ptr, size));
-    host_ptr = (int*)malloc(size);
+  int *host_ptr = nullptr;
+  int *dev_ptr = nullptr;
+  size_t size = sizeof(int);
+  CHECK(hipMalloc(&dev_ptr, size));
+  host_ptr = (int *)malloc(size);
 
-    // Push a 10sec long taks into the stream
-    CHECK(hipStreamAddCallback(stream_non_blocking, callback_sleep10, stream_shared_data, 0));
+  // Push a 10sec long taks into the stream
+  CHECK(hipStreamAddCallback(stream_non_blocking, callback_sleep10,
+                             stream_shared_data, 0));
 
-    //printf("Starting task on null stream\n");
-    *host_ptr = 100; // init value
-    CHECK(hipMemcpy(dev_ptr, host_ptr, size, hipMemcpyDefault));
-    hipLaunchKernelGGL(addOne, 1, 1, 0, 0, dev_ptr);
-    CHECK(hipGetLastError());
-    CHECK(hipMemcpyAsync(host_ptr, dev_ptr, size, hipMemcpyDefault));
-    CHECK(hipStreamSynchronize(0));
-    //printf("End of null stream task\n");fflush(stdout);
+  // printf("Starting task on null stream\n");
+  *host_ptr = 100; // init value
+  CHECK(hipMemcpy(dev_ptr, host_ptr, size, hipMemcpyDefault));
+  hipLaunchKernelGGL(addOne, 1, 1, 0, 0, dev_ptr);
+  CHECK(hipGetLastError());
+  CHECK(hipMemcpyAsync(host_ptr, dev_ptr, size, hipMemcpyDefault));
+  CHECK(hipStreamSynchronize(0));
+  // printf("End of null stream task\n");fflush(stdout);
 
-    bool testStatus = true;
-    printf("%s (non-blocking stream): ", __FUNCTION__);
-    if (*host_ptr == 101 && *stream_shared_data == 2) {
-        testStatus = false;
-        printf("%s %s %s\n","\033[0;31m", "Failed", "\033[0m");
-        //printf("host_ptr = %d, stream_shared_data = %d\n", *host_ptr, *stream_shared_data);fflush(stdout);
-    } else {
-        printf("Passed\n");
-    }
-    
-    // Wait for all tasks to be finished
-    CHECK(hipDeviceSynchronize());
+  bool testStatus = true;
+  printf("%s (non-blocking stream): ", __FUNCTION__);
+  if (*host_ptr == 101 && *stream_shared_data == 2) {
+    testStatus = false;
+    printf("%s %s %s\n", "\033[0;31m", "Failed", "\033[0m");
+    // printf("host_ptr = %d, stream_shared_data = %d\n", *host_ptr,
+    // *stream_shared_data);fflush(stdout);
+  } else {
+    printf("Passed\n");
+  }
 
-    // Clean-up
-    CHECK(hipStreamDestroy(stream_non_blocking));
-    CHECK(hipFree(dev_ptr));
-    free(stream_shared_data);
-    free(host_ptr);
-    return testStatus;
+  // Wait for all tasks to be finished
+  CHECK(hipDeviceSynchronize());
+
+  // Clean-up
+  CHECK(hipStreamDestroy(stream_non_blocking));
+  CHECK(hipFree(dev_ptr));
+  free(stream_shared_data);
+  free(host_ptr);
+  return testStatus;
 }
 
 /*
  * Intent : Verify streams work independently
  */
 bool TestStreamSemantics_3() {
-    printf("------------------------------------------------------------------\n");
-    int* stream1_shared_data = nullptr;
-    int* stream2_shared_data = nullptr;
-    stream1_shared_data = (int*)malloc(sizeof(int));
-    stream2_shared_data = (int*)malloc(sizeof(int));
-    hipStream_t stream1, stream2;
-    CHECK(hipStreamCreate(&stream1));
-    CHECK(hipStreamCreate(&stream2));
+  printf(
+      "------------------------------------------------------------------\n");
+  int *stream1_shared_data = nullptr;
+  int *stream2_shared_data = nullptr;
+  stream1_shared_data = (int *)malloc(sizeof(int));
+  stream2_shared_data = (int *)malloc(sizeof(int));
+  hipStream_t stream1, stream2;
+  CHECK(hipStreamCreate(&stream1));
+  CHECK(hipStreamCreate(&stream2));
 
-    *stream1_shared_data = 1;
-    CHECK(hipStreamAddCallback(stream1, callback_sleep2, stream1_shared_data, 0));
+  *stream1_shared_data = 1;
+  CHECK(hipStreamAddCallback(stream1, callback_sleep2, stream1_shared_data, 0));
 
-    int* dev_ptr = nullptr;
-    CHECK(hipMalloc(&dev_ptr, sizeof(int)));
-    hipLaunchKernelGGL(addOne, 1, 1, 0, 0, dev_ptr);
-    CHECK(hipGetLastError());
+  int *dev_ptr = nullptr;
+  CHECK(hipMalloc(&dev_ptr, sizeof(int)));
+  hipLaunchKernelGGL(addOne, 1, 1, 0, 0, dev_ptr);
+  CHECK(hipGetLastError());
 
-    *stream2_shared_data = 1;
-    CHECK(hipStreamAddCallback(stream2, callback_sleep10, stream2_shared_data, 0));
+  *stream2_shared_data = 1;
+  CHECK(
+      hipStreamAddCallback(stream2, callback_sleep10, stream2_shared_data, 0));
 
-    printf("Going to sync stream1\n");
-    CHECK(hipStreamSynchronize(stream1));
-    printf("Going to call query\n");
+  printf("Going to sync stream1\n");
+  CHECK(hipStreamSynchronize(stream1));
+  printf("Going to call query\n");
 
-    hipError_t status = hipStreamQuery(stream2);
-    bool testStatus = true;
-    printf("%s(independent execution) : ", __FUNCTION__);
-    if (status != hipErrorNotReady) {
-        printf("%s %s %s\n","\033[0;31m", "Failed", "\033[0m");
-        testStatus = false;
-        //printf("Failed, queue status is %s but expected is hipErrorNotReady\n", hipGetErrorName(status));
-    } else {
-        printf("Passed\n");
-    }
+  hipError_t status = hipStreamQuery(stream2);
+  bool testStatus = true;
+  printf("%s(independent execution) : ", __FUNCTION__);
+  if (status != hipErrorNotReady) {
+    printf("%s %s %s\n", "\033[0;31m", "Failed", "\033[0m");
+    testStatus = false;
+    // printf("Failed, queue status is %s but expected is hipErrorNotReady\n",
+    // hipGetErrorName(status));
+  } else {
+    printf("Passed\n");
+  }
 
-    CHECK(hipDeviceSynchronize());
-    
-    // clean-up
-    CHECK(hipStreamDestroy(stream1));
-    CHECK(hipStreamDestroy(stream2));
-    free(stream1_shared_data);
-    free(stream2_shared_data);
+  CHECK(hipDeviceSynchronize());
 
-    return testStatus;
+  // clean-up
+  CHECK(hipStreamDestroy(stream1));
+  CHECK(hipStreamDestroy(stream2));
+  free(stream1_shared_data);
+  free(stream2_shared_data);
+
+  return testStatus;
 }
 
 /*
  * Intent : Destroy a stream with pending task
- * Expectation : Test should end gracefully, there should not be any hang or crash.
+ * Expectation : Test should end gracefully, there should not be any hang or
+ * crash.
  */
 bool TestStreamSemantics_4() {
-    printf("------------------------------------------------------------------\n");
-    int* stream_shared_data = nullptr;
-    stream_shared_data = (int*)malloc(sizeof(int));
-    *stream_shared_data = 1;
+  printf(
+      "------------------------------------------------------------------\n");
+  int *stream_shared_data = nullptr;
+  stream_shared_data = (int *)malloc(sizeof(int));
+  *stream_shared_data = 1;
 
-    hipStream_t stream;
-    CHECK(hipStreamCreate(&stream));
-    CHECK(hipStreamAddCallback(stream, callback_sleep2, stream_shared_data, 0));
-    printf("Going to destroy a stream which has pending work\n");
-    CHECK(hipStreamDestroy(stream));
+  hipStream_t stream;
+  CHECK(hipStreamCreate(&stream));
+  CHECK(hipStreamAddCallback(stream, callback_sleep2, stream_shared_data, 0));
+  printf("Going to destroy a stream which has pending work\n");
+  CHECK(hipStreamDestroy(stream));
 
-    printf("Waiting for device to finish the task\n");
-    CHECK(hipDeviceSynchronize());
+  printf("Waiting for device to finish the task\n");
+  CHECK(hipDeviceSynchronize());
 
-    printf("%s (stream destroy): Passed\n", __FUNCTION__);
+  printf("%s (stream destroy): Passed\n", __FUNCTION__);
 
-    free(stream_shared_data);
-    return true;
+  free(stream_shared_data);
+  return true;
 }
 
 /*
- * Intent : Synchronize between two streams using event 
+ * Intent : Synchronize between two streams using event
  */
 bool TestStreamSemantics_5() {
-    printf("------------------------------------------------------------------\n");
-    int* stream_shared_data = nullptr;
-    stream_shared_data = (int*)malloc(sizeof(int));
-    *stream_shared_data = 1;
+  printf(
+      "------------------------------------------------------------------\n");
+  int *stream_shared_data = nullptr;
+  stream_shared_data = (int *)malloc(sizeof(int));
+  *stream_shared_data = 1;
 
-    hipStream_t stream1, stream2;
-    hipEvent_t event;
-    CHECK(hipStreamCreate(&stream1));
-    CHECK(hipStreamCreate(&stream2));
-    CHECK(hipEventCreate(&event));
+  hipStream_t stream1, stream2;
+  hipEvent_t event;
+  CHECK(hipStreamCreate(&stream1));
+  CHECK(hipStreamCreate(&stream2));
+  CHECK(hipEventCreate(&event));
 
-    CHECK(hipStreamAddCallback(stream1, callback_sleep2, stream_shared_data, 0));
-    CHECK(hipEventRecord(event, stream1));
+  CHECK(hipStreamAddCallback(stream1, callback_sleep2, stream_shared_data, 0));
+  CHECK(hipEventRecord(event, stream1));
 
-    CHECK(hipStreamWaitEvent(stream2, event, 0));
-    CHECK(hipStreamSynchronize(stream2));
+  CHECK(hipStreamWaitEvent(stream2, event, 0));
+  CHECK(hipStreamSynchronize(stream2));
 
-    bool testStatus = true;
-    printf("%s (sync streams): ", __FUNCTION__);
-    if (*stream_shared_data != 2) {
-        testStatus = false;
-        printf("%s %s %s\n","\033[0;31m", "Failed", "\033[0m");
-    } else {
-        printf("Passed\n");
-    }
-    
-    CHECK(hipDeviceSynchronize());
+  bool testStatus = true;
+  printf("%s (sync streams): ", __FUNCTION__);
+  if (*stream_shared_data != 2) {
+    testStatus = false;
+    printf("%s %s %s\n", "\033[0;31m", "Failed", "\033[0m");
+  } else {
+    printf("Passed\n");
+  }
 
-    CHECK(hipStreamDestroy(stream1));
-    CHECK(hipStreamDestroy(stream2));
-    free(stream_shared_data);
-    return testStatus;
+  CHECK(hipDeviceSynchronize());
+
+  CHECK(hipStreamDestroy(stream1));
+  CHECK(hipStreamDestroy(stream2));
+  free(stream_shared_data);
+  return testStatus;
 }
 
 int main() {
-    TestStreamSemantics_1();
-    TestStreamSemantics_2();
-    TestStreamSemantics_3();
-    TestStreamSemantics_4();
-    TestStreamSemantics_5();
-    return 0;
+  TestStreamSemantics_1();
+  TestStreamSemantics_2();
+  TestStreamSemantics_3();
+  TestStreamSemantics_4();
+  TestStreamSemantics_5();
+  return 0;
 }
