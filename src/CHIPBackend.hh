c81d0afc5ac188605af1c54f79c7288e8f95850e
Paulius Velesko <pvelesko@pglc.io>
2022-11-08 14:32:47 +0000

    fmt

--- src/CHIPBackend.hh
+++ src/CHIPBackend.hh
@@ -1142,7 +1142,7 @@ public:
   std::mutex DeviceVarMtx;
   std::mutex DeviceMtx;
 
-  CHIPQueue* LegacyDefaultQueue;
+  CHIPQueue *LegacyDefaultQueue;
   inline static thread_local std::unique_ptr<CHIPQueue> PerThreadDefaultQueue;
 
   /**
