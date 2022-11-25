c5ba75c3c5634b7247095e37e47444c7e022c6bf
Henry Linjamäki <henry.mikael.linjamaki@intel.com>
2022-11-25 13:10:15 +0200

    Fix compiler error with __attribute__((__noinline__))
    Fix a compiler error caused by C++ libraries (e.g. libstdc++ provided
    by g++-12) using `__attribute__((__noinline__))` in their code. Our
    definition of the `__noinline__` appears in the compilation as followed:
    
    ```
    /usr/lib/gcc/x86_64-linux-gnu/12/../../../../include/c++/12/bits/shared_ptr_base.h:196:37: error: use of undeclared identifier 'noinline'; did you mean 'inline'?
          __attribute__((__attribute__((noinline))))
    ...
    ```
    
    For Clang <15 this issue is fixed by substituting the `__noinline__` with
    empty string and, therefore, rendering it ineffective, unfortunately.
    
    Fortunately, In Clang >=15 the `__noinline__` is a builtin keyword
    and the following use cases work in CUDA and HIP language modes:
    
    ```
    __attribute__((__inline__)) void foo(...);
    __inline__ void bar(...);
    ```

--- include/hip/spirv_hip_host_defines.h
+++ include/hip/spirv_hip_host_defines.h
@@ -25,6 +25,13 @@
 
 #if defined(__clang__) && defined(__HIP__)
 
+// Undefine the __noinline__ coming from the CHIP-SPV's fork of the
+// HIP-Common as it is shadowing the keyword in Clang 15+. The
+// upstream HIP has removed the __noinline__ definition and also the
+// other HIP function attributes and let the implementers define them.
+// TODO: We should probably remove the HIP attributes in the HIP-Common fork.
+#undef __noinline__
+
 #if !__CLANG_HIP_RUNTIME_WRAPPER_INCLUDED__
 #define __host__ __attribute__((host))
 #define __device__ __attribute__((device))
@@ -33,7 +40,15 @@
 #define __constant__ __attribute__((constant))
 #endif  // !__CLANG_HIP_RUNTIME_WRAPPER_INCLUDED__
 
-#define __noinline__ __attribute__((noinline))
+#if !defined(__has_feature) || !__has_feature(cuda_noinline_keyword)
+// In Clang 15+ __noinline__ is a keyword which works within
+// __attribute__(()) and standalone. For the earlier Clang versions (and
+// compilers which don't recognize the keyword) we have to substitute
+// it to empty string so C++ code using __attribute__((__noinline__))
+// compiles.
+#define __noinline__
+#endif
+
 #define __forceinline__ inline __attribute__((always_inline))
 
 #else
