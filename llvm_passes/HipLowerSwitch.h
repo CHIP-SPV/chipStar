3b40bfa440911c23f86859cdefef9e5812728220
Henry Linjamäki <henry.mikael.linjamaki@intel.com>
2022-12-16 11:33:48 +0200

    Fix llvm-spirv crashes on a switch instruction
    ... with "non-standard" integer bitwidth used as switch value (e.g. i4).

---
+++ llvm_passes/HipLowerSwitch.h
@@ -0,0 +1,27 @@
+//===- HipLowerSwitch.h ---------------------------------------------------===//
+//
+// Part of the CHIP-SPV Project, under the Apache License v2.0 with LLVM
+// Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+// Lowers switch instructions to be suitable for llvm-spirv.
+//
+// (c) 2023 CHIP-SPV Developers
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_PASSES_HIP_LOWER_SWITCH_H
+#define LLVM_PASSES_HIP_LOWER_SWITCH_H
+
+#include "llvm/IR/PassManager.h"
+
+using namespace llvm;
+
+class HipLowerSwitchPass : public PassInfoMixin<HipLowerSwitchPass> {
+public:
+  PreservedAnalyses run(Function &M, FunctionAnalysisManager &AM);
+  static bool isRequired() { return true; }
+};
+
+#endif
