a2f2135c9ca8354d0f9598a7b83015d5e00a02dc
Paulius Velesko <pvelesko@pglc.io>
2023-01-12 08:49:39 +0000

    fix C compat sample

--- include/hip/spirv_hip_runtime.h
+++ include/hip/spirv_hip_runtime.h
@@ -38,7 +38,6 @@ THE SOFTWARE.
 
 #include <hip/spirv_hip_vector_types.h>
 
-//#include <hip/spirv_hip_ldg.h>
 #include <hip/spirv_math_fwd.h>
 
 #include <hip/spirv_texture_functions.h>
--- samples/ccompat/ccompat.c
+++ samples/ccompat/ccompat.c
@@ -1,43 +1,15 @@
 #include <stdio.h>
-#include <hip/hip_runtime.h>
+#include "hip/hip_runtime.h"
+#include "hip/hip_runtime_api.h"
 
-__global__
-void saxpy(int n, float a, float *x, float *y)
-{
-  int i = blockIdx.x*blockDim.x + threadIdx.x;
-  if (i < n) y[i] = a*x[i] + y[i];
-}
 
-int main(void)
-{
-  int N = 1<<20;
-  float *x, *y, *d_x, *d_y;
-  x = (float*)malloc(N*sizeof(float));
-  y = (float*)malloc(N*sizeof(float));
+extern __global__ void saxpy();
 
-  hipMalloc(&d_x, N*sizeof(float)); 
-  hipMalloc(&d_y, N*sizeof(float));
+  dim3 blocks 	    = {1,1,1};
+  dim3 threads      = {1,1,1};
 
-  for (int i = 0; i < N; i++) {
-    x[i] = 1.0f;
-    y[i] = 2.0f;
-  }
-
-  hipMemcpy(d_x, x, N*sizeof(float), hipMemcpyHostToDevice);
-  hipMemcpy(d_y, y, N*sizeof(float), hipMemcpyHostToDevice);
-
-  // Perform SAXPY on 1M elements
-  saxpy<<<(N+255)/256, 256>>>(N, 2.0f, d_x, d_y);
-
-  hipMemcpy(y, d_y, N*sizeof(float), hipMemcpyDeviceToHost);
-
-  float maxError = 0.0f;
-  for (int i = 0; i < N; i++)
-    maxError = std::max(maxError, std::abs(y[i]-4.0f));
-  printf("Max error: %f\n", maxError);
-
-  hipFree(d_x);
-  hipFree(d_y);
-  free(x);
-  free(y);
+int main(void) {
+  hipLaunchKernel((const void*)(&saxpy), blocks, threads, 0, 0, 0);
+  hipDeviceSynchronize();
+  return 0;
 }
