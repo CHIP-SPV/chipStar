87a727018cd4fc2f2c877e7b6f3ea00922a7b7e2
Paulius Velesko <pvelesko@pglc.io>
2023-01-21 06:43:38 +0200

    CI upgrade (#317)
    * move test exclusions to separate file
    
    * cmake call git submodule init
    
    * remove unit test logs
    
    * Update failed tests
    
    * unit tests in parallel
    
    * Setup env vars for tests
    
    * setup env vars
    
    * More failed tests
    
    * more failed tests
    
    * more failed tests
    
    * more failed

--- .github/scripts/check_unit_tests.sh
+++ .github/scripts/check_unit_tests.sh
@@ -14,15 +14,25 @@ make -j
 make build_tests -j
 
 # Test OpenCL iGPU
-CHIP_PLATFORM=4 CHIP_DEVICE_TYPE=gpu CHIP_BE=opencl make check | tee igpu_opencl_make_check_result.txt
+echo "begin igpu_opencl_failed_tests"
+CHIP_BE=opencl CHIP_DEVICE_TYPE=gpu CHIP_PLATFORM=4 CHIP_DEVICE=0 ctest --timeout 180 -j 8 -E "`cat ./test_lists/igpu_opencl_failed_tests.txt`" | tee igpu_opencl_make_check_result.txt
+echo "end igpu_opencl_failed_tests"
 # Test OpenCL dGPU
-CHIP_PLATFORM=3 CHIP_DEVICE_TYPE=gpu CHIP_BE=opencl make check | tee dgpu_opencl_make_check_result.txt
+echo "begin dgpu_opencl_failed_tests"
+CHIP_BE=opencl CHIP_DEVICE_TYPE=gpu CHIP_PLATFORM=3 CHIP_DEVICE=0 ctest --timeout 180 -j 8 -E "`cat ./test_lists/dgpu_opencl_failed_tests.txt`" | tee dgpu_opencl_make_check_result.txt
+echo "end dgpu_opencl_failed_tests"
 # Test OpenCL CPU
-CHIP_PLATFORM=1 CHIP_DEVICE_TYPE=cpu CHIP_BE=opencl make check | tee cpu_opencl_make_check_result.txt
+echo "begin cpu_opencl_failed_tests"
+CHIP_BE=opencl CHIP_DEVICE_TYPE=cpu CHIP_PLATFORM=1 CHIP_DEVICE=0 ctest --timeout 180 -j 8 -E "`cat ./test_lists/cpu_opencl_failed_tests.txt`" | tee cpu_opencl_make_check_result.txt
+echo "end cpu_opencl_failed_tests"
 # Test Level Zero iGPU
-CHIP_DEVICE_TYPE=gpu CHIP_DEVICE=1 CHIP_BE=level0 make check | tee igpu_level0_make_check_result.txt
+echo "begin igpu_level0_failed_tests"
+CHIP_BE=level0 CHIP_DEVICE=1 ctest --timeout 180 -j 1 -E "`cat ./test_lists/igpu_level0_failed_tests.txt`" | tee igpu_level0_make_check_result.txt
+echo "end igpu_level0_failed_tests"
 # Test Level Zero dGPU
-CHIP_DEVICE_TYPE=gpu CHIP_DEVICE=0 CHIP_BE=level0 make check | tee dgpu_level0_make_check_result.txt
+echo "begin dgpu_level0_failed_tests"
+CHIP_BE=level0 CHIP_DEVICE=0 ctest --timeout 180 -j 1 -E "`cat ./test_lists/dgpu_level0_failed_tests.txt`" | tee dgpu_level0_make_check_result.txt
+echo "end dgpu_level0_failed_tests"
 
 
 if [[ `cat igpu_opencl_make_check_result.txt | grep "0 tests failed out of"` ]]
