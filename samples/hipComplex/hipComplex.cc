aa0161ec2a0f90766d61b089b2276a10a20dbdb4
Paulius Velesko <pvelesko@pglc.io>
2023-02-27 16:03:40 +0200

    hipComplex (#358)
    * Copy over hipamd hipcomplex
    
    * adjust include
    
    * add a hipComplex sample
    
    * sample regex
    
    * fmt

---
+++ samples/hipComplex/hipComplex.cc
@@ -0,0 +1,18 @@
+#include <hip/hip_runtime.h>
+#include <hip/hip_complex.h>
+
+int main(int argc, char **argv) {
+  hipDoubleComplex x = make_hipDoubleComplex(2.0, 3.0);
+  hipDoubleComplex y = make_hipDoubleComplex(4.0, 2.0);
+
+  y = y * x;
+
+  printf("%f%+fi\n", hipCreal(y), hipCimag(y));
+  if (hipCreal(y) == 2.0 && hipCimag(y) == 16.0) {
+    printf("PASSED");
+  } else {
+    printf("FAILED");
+  }
+
+  return 0;
+}
