93110b5620c8392bf5dd6272ef0463b0651d24a2
Henry Linjamäki <linehill@users.noreply.github.com>
2023-02-28 07:38:32 +0200

    Implement missing __shfl* overloads (#366)
    * Implement missing __shfl* overloads
    
    Fixes #364.
    
    * Drop shuffle testing for double type
    
    (testing if this appeases the CI due to lack of double type support)
    
    * Add a separate test for double shuffles
    
    ---------
    
    Co-authored-by: Henry Linjamäki <henry.mikael.linjamaki@intel.com>

--- samples/shuffles/shuffles.cc
+++ samples/shuffles/shuffles.cc
@@ -31,64 +31,67 @@
 
 #include "hip/hip_runtime.h"
 
-__global__ void shfl_down_int(int *In, int *Out, int Delta, int Width) {
+template <typename T>
+__global__ void shfl_down(T *In, T *Out, int Delta, int Width) {
   Out[threadIdx.x] = __shfl_down(In[threadIdx.x], Delta, Width);
 }
 
-__global__ void shfl_up_int(int *In, int *Out, int Delta, int Width) {
+template <typename T>
+__global__ void shfl_up(T *In, T *Out, int Delta, int Width) {
   Out[threadIdx.x] = __shfl_up(In[threadIdx.x], Delta, Width);
 }
 
-__global__ void shfl_int(int *In, int *Out, int Lane, int Width) {
+template <typename T> __global__ void shfl(T *In, T *Out, int Lane, int Width) {
   Out[threadIdx.x] = __shfl(In[threadIdx.x], Lane, Width);
 }
 
-__global__ void shfl_down_float(float *In, float *Out, int Delta, int Width) {
-  Out[threadIdx.x] = __shfl_down(In[threadIdx.x], Delta, Width);
-}
-
-__global__ void shfl_up_float(float *In, float *Out, int Delta, int Width) {
-  Out[threadIdx.x] = __shfl_up(In[threadIdx.x], Delta, Width);
-}
-
-__global__ void shfl_float(float *In, float *Out, int Lane, int Width) {
-  Out[threadIdx.x] = __shfl(In[threadIdx.x], Lane, Width);
+template <typename T>
+__global__ void shfl_xor(T *In, T *Out, int Mask, int Width) {
+  Out[threadIdx.x] = __shfl_xor(In[threadIdx.x], Mask, Width);
 }
 
 __global__ void test_lane_id(unsigned *Out) { Out[threadIdx.x] = __lane_id(); }
 
 #define LAUNCH_CASE_T(TYPE, KERNEL, DELTA, WIDTH)                              \
-  std::iota(Input, Input + Threads, (TYPE)0);                                  \
-  std::fill(Output, Output + Threads, (TYPE)0);                                \
+  std::iota((TYPE *)Input, (TYPE *)Input + Threads, (TYPE)0);                  \
+  std::fill((TYPE *)Output, (TYPE *)Output + Threads, (TYPE)0);                \
                                                                                \
-  hipLaunchKernelGGL(KERNEL##_##TYPE, dim3(CHIP_DEFAULT_WARP_SIZE),            \
+  hipLaunchKernelGGL(KERNEL<TYPE>, dim3(CHIP_DEFAULT_WARP_SIZE),               \
                      dim3(Threads), 0, 0, (TYPE *)Input, (TYPE *)Output,       \
                      DELTA, WIDTH);                                            \
-  hipStreamSynchronize(0);                                                     \
+  (void)hipStreamSynchronize(0);                                               \
                                                                                \
-  std::cout << #KERNEL "_" #TYPE << " arg=" << DELTA << " width=" << WIDTH     \
+  std::cout << #KERNEL "<" #TYPE << "> arg=" << DELTA << " width=" << WIDTH    \
             << ": " << std::endl;                                              \
   for (int i = 0; i < Threads;) {                                              \
     for (int wi = 0; wi < WIDTH; ++wi, ++i)                                    \
-      std::cout << (TYPE)Output[i] << ' ';                                     \
+      std::cout << ((TYPE *)Output)[i] << ' ';                                 \
     std::cout << std::endl;                                                    \
   }                                                                            \
   std::cout << std::endl
 
 #define LAUNCH_CASE(KERNEL, DELTA, WIDTH)                                      \
   LAUNCH_CASE_T(int, KERNEL, DELTA, WIDTH);                                    \
-  LAUNCH_CASE_T(float, KERNEL, DELTA, WIDTH)
+  LAUNCH_CASE_T(unsigned int, KERNEL, DELTA, WIDTH);                           \
+  LAUNCH_CASE_T(long, KERNEL, DELTA, WIDTH);                                   \
+  LAUNCH_CASE_T(unsigned long, KERNEL, DELTA, WIDTH);                          \
+  LAUNCH_CASE_T(long long, KERNEL, DELTA, WIDTH);                              \
+  LAUNCH_CASE_T(unsigned long long, KERNEL, DELTA, WIDTH);                     \
+  LAUNCH_CASE_T(float, KERNEL, DELTA, WIDTH);
+// Skipping testing for double type as the CI fails on shuffles with
+// double argument.
 
 int main(int argc, char *argv[]) {
 
-  uint Threads = CHIP_DEFAULT_WARP_SIZE * 2;
+  constexpr uint Threads = CHIP_DEFAULT_WARP_SIZE * 2;
+  constexpr unsigned MaxEltSize = sizeof(double);
 
   void *OutputVoid;
-  hipHostMalloc(&OutputVoid, sizeof(int) * Threads);
+  hipHostMalloc(&OutputVoid, MaxEltSize * Threads);
   auto Output = reinterpret_cast<int *>(OutputVoid);
 
   void *InputVoid;
-  hipHostMalloc(&InputVoid, sizeof(int) * Threads);
+  hipHostMalloc(&InputVoid, MaxEltSize * Threads);
   auto Input = reinterpret_cast<int *>(InputVoid);
 
   LAUNCH_CASE(shfl_down, 4, CHIP_DEFAULT_WARP_SIZE);
@@ -102,6 +105,9 @@ int main(int argc, char *argv[]) {
 
   LAUNCH_CASE(shfl, 15, 16);
   LAUNCH_CASE(shfl, 0, 16);
+
+  LAUNCH_CASE(shfl_xor, 3, CHIP_DEFAULT_WARP_SIZE);
+  LAUNCH_CASE(shfl_xor, 5, CHIP_DEFAULT_WARP_SIZE);
 
   std::fill(Output, Output + Threads, 0);
 
