829c825cf648b518a5dae7a5a16359f54eed434c
Paulius Velesko <pvelesko@pglc.io>
2023-04-20 10:01:35 -0500

    fmt

--- tests/compiler/TestHipVersion.cc
+++ tests/compiler/TestHipVersion.cc
@@ -1,14 +1,13 @@
 #include <hip/hip_runtime.h>
 
 __global__ void kernel() {
-    printf("%d", HIP_VERSION_MAJOR);
-    printf("%d", HIP_VERSION_MINOR);
-    printf("%d", HIP_VERSION_PATCH);
+  printf("%d", HIP_VERSION_MAJOR);
+  printf("%d", HIP_VERSION_MINOR);
+  printf("%d", HIP_VERSION_PATCH);
 }
 
 int main() {
-    kernel<<<1, 1>>>();
-    hipDeviceSynchronize();
-    return 0;
+  kernel<<<1, 1>>>();
+  hipDeviceSynchronize();
+  return 0;
 }
-
