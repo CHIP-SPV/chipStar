438845767d44e5fa9f22c01bee57efcbce4af2a9
Paulius Velesko <pvelesko@pglc.io>
2023-05-05 05:23:16 -0500

    spir-v dump fixes

--- src/Utils.cc
+++ src/Utils.cc
@@ -42,14 +42,34 @@ std::string readEnvVar(std::string EnvVar, bool Lower) {
   return Var;
 }
 
+std::string generateShortHash(std::string_view input, size_t length) {
+    std::hash<std::string_view> hasher;
+    std::size_t hashValue = hasher(input);
+
+    std::stringstream ss;
+    ss << std::hex << std::setw(length) << std::setfill('0') << hashValue;
+
+    return ss.str().substr(0, length);
+}
+
 /// Dump the SPIR-V to a file
 void dumpSpirv(std::string_view Spirv) {
-  auto dump = readEnvVar("CHIP_DUMP_SPIRV");
-  if(dump.empty())
-    return;
-  std::ofstream SpirvFile("hip-spirv-failed.spv", std::ios::binary);
-  SpirvFile.write(Spirv.data(), Spirv.size());
-  SpirvFile.close();
+    auto dump = readEnvVar("CHIP_DUMP_SPIRV");
+    if (dump.empty()) {
+        return;
+    }
+
+    std::string hashSum = generateShortHash(Spirv, 6);
+    std::string fileName = "hip-spirv-" + hashSum + ".spv";
+    std::ofstream SpirvFile(fileName, std::ios::binary);
+
+    if (!SpirvFile) {
+        std::cerr << "Error: Could not open file " << fileName << " for writing" << std::endl;
+        return;
+    }
+
+    SpirvFile.write(Spirv.data(), Spirv.size());
+    SpirvFile.close();
 }
 
 /// Returns true if the file can be executed.
