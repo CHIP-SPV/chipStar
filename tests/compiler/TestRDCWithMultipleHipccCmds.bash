33a684759fb9d75636137aa2debf2e3dfd307e3d
Henry Linjamäki <linehill@users.noreply.github.com>
2023-05-06 14:04:57 +0300

    Revive RDC mode - part two (#377)
    * fix 423
    
    * RDC: propagate supplement RDC link flags to .hipInfo
    
    * RDC: Add hipcc guide for RDC mode
    
    * RDC: Test RDC and non-RDC compilation with hipcc
    
    * fix rdc hipcc
    
    * re-enable resolved hipcc tests
    
    ---------
    
    Co-authored-by: Paulius Velesko <pvelesko@pglc.io>

---
+++ tests/compiler/TestRDCWithMultipleHipccCmds.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Test RDC mode with separate compile and link steps.
+set -eu
+
+SRC_DIR=@CMAKE_CURRENT_SOURCE_DIR@
+OUT_DIR=@CMAKE_CURRENT_BINARY_DIR@/@TEST_NAME@.d
+HIPCC=@CMAKE_BINARY_DIR@/bin/hipcc.bin
+
+mkdir -p ${OUT_DIR}
+# Also check hipcc is not issuing misplaced options.
+${HIPCC} -Werror=unused-command-line-argument -fgpu-rdc \
+         ${SRC_DIR}/inputs/a.hip -c -o ${OUT_DIR}/a.o
+${HIPCC} -Werror=unused-command-line-argument -fgpu-rdc \
+         ${SRC_DIR}/inputs/b.hip -c -o ${OUT_DIR}/b.o
+${HIPCC} -Werror=unused-command-line-argument \
+         -fgpu-rdc ${OUT_DIR}/{a,b}.o -o ${OUT_DIR}/ab
+${OUT_DIR}/ab
---
+++ tests/compiler/TestWholeProgramCompilation.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Check __device__ functions and variables by the same name in multiple
+# TUs are not attempted to be linked together, when -fgpu-rdc option
+# is absent, and they are not mixed up at the runtime.
+set -eu
+
+SRC_DIR=@CMAKE_CURRENT_SOURCE_DIR@
+OUT_DIR=@CMAKE_CURRENT_BINARY_DIR@/@TEST_NAME@.d
+HIPCC=@CMAKE_BINARY_DIR@/bin/hipcc.bin
+
+mkdir -p ${OUT_DIR}
+# Also check hipcc is not issuing misplaced options.
+${HIPCC} -Werror=unused-command-line-argument \
+         ${SRC_DIR}/inputs/foobar{,-main}.hip -o ${OUT_DIR}/foobar
+${OUT_DIR}/foobar
+
+${HIPCC} -Werror=unused-command-line-argument \
+         ${SRC_DIR}/inputs/foobar.hip -c -o ${OUT_DIR}/foobar.o
+${HIPCC} -Werror=unused-command-line-argument \
+         ${SRC_DIR}/inputs/foobar-main.hip -c -o ${OUT_DIR}/foobar-main.o
+${HIPCC} -Werror=unused-command-line-argument \
+         ${OUT_DIR}/foobar{,-main}.o -o /dev/null
+
+${HIPCC} -Werror=unused-command-line-argument -fno-gpu-rdc \
+         ${SRC_DIR}/inputs/foobar{,-main}.hip -o /dev/null
