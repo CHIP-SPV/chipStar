2f6d05d554677bcb14bb4c8f8b827a13f905b437
Paulius Velesko <pvelesko@pglc.io>
2023-06-02 05:24:25 -0500

    check.py hotfix

--- scripts/check.py
+++ scripts/check.py
@@ -20,10 +20,13 @@ def process_args():
     work_dir = sys.argv[1]
     if sys.argv[2] == "cpu":
         device_type = "cpu"
+        device_type_stripped = "cpu"
     elif sys.argv[2] == "dgpu":
-        device_type = "gpu"
+        device_type = "dgpu"
+        device_type_stripped = "gpu"
     elif sys.argv[2] == "igpu":
-        device_type = "gpu"
+        device_type = "igpu"
+        device_type_stripped = "gpu"
     else:
         print("Unrecognized device type: " + sys.argv[2])
         print(usage_line)
@@ -40,21 +43,21 @@ def process_args():
         sys.exit(1)
     num_threads = sys.argv[4] 
     num_tries = sys.argv[5]
-    return work_dir, device_type, backend, num_threads, num_tries
+    if(backend == "pocl" or backend == "opencl"):
+        env_vars = "CHIP_BE=opencl CHIP_DEVICE_TYPE={device_type_stripped}".format(backend=backend, device_type_stripped=device_type_stripped)
+    else:
+        env_vars = "CHIP_BE=level0 CHIP_DEVICE_TYPE={device_type_stripped}".format(backend=backend, device_type_stripped=device_type_stripped)
+    if (device_type == "cpu"):
+        timeout = 400
+    else:
+        timeout = 180
+    return work_dir, device_type, backend, num_threads, num_tries, timeout, env_vars
 
 resolved_tests = {}
 
-work_dir, device_type, backend, num_threads, num_tries = process_args()
-if (device_type == "cpu"):
-    timeout = 400
-else:
-    timeout = 180
+work_dir, device_type, backend, num_threads, num_tries, timeout, env_vars = process_args()
 
 os.chdir(work_dir)
-if(backend == "pocl" or backend == "opencl"):
-    env_vars = "CHIP_BE=opencl CHIP_DEVICE_TYPE={device_type}".format(backend=backend, device_type=device_type)
-else:
-    env_vars = "CHIP_BE=level0 CHIP_DEVICE_TYPE={device_type}".format(backend=backend, device_type=device_type)
 
 cmd = "{env_vars} ctest --output-on-failure --timeout {timeout} --repeat until-fail:{num_tries} -j {num_threads} -E \"`cat ./test_lists/{device_type}_{backend}_failed_tests.txt`\"".format(work_dir=work_dir, num_tries=num_tries, env_vars=env_vars, num_threads=num_threads, device_type=device_type, backend=backend, timeout=timeout)
 run_cmd(cmd)
