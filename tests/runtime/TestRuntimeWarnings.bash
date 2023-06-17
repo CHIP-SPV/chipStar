23e870c04328168d58f9e0ff24fccd73d5e798ff
Henry Linjamäki <linehill@users.noreply.github.com>
2023-06-17 10:09:34 +0300

    Don't emit missing definition warnings for builtins (#507)
    Closes #481, #505.

---
+++ tests/runtime/TestRuntimeWarnings.bash
@@ -0,0 +1,27 @@
+# A regression test for https://github.com/CHIP-SPV/chip-spv/issues/481.
+#!/bin/bash
+set -eu
+
+SRC_DIR=@CMAKE_CURRENT_SOURCE_DIR@
+OUT_DIR=@CMAKE_CURRENT_BINARY_DIR@/@TEST_NAME@.d
+HIPCC=@CMAKE_BINARY_DIR@/bin/hipcc.bin
+
+mkdir -p ${OUT_DIR}
+cd ${OUT_DIR}
+
+${HIPCC} ${SRC_DIR}/inputs/MissingDef.hip -o missing-def
+
+# Executable may fail or crash - that's expected.
+CHIP_LOGLEVEL=warn ./missing-def >output.log 2>&1 || true
+
+# Should not display warning on symbols wich are decorated with
+# 'BuiltIn' attribute.
+! grep -c "warning .* Missing definition for '__spirv_BuiltIn" output.log ||  {
+    echo "FAIL: unexpected warning was seen."
+    exit 1
+}
+
+grep -c "warning .* Missing definition for '_Z3fooi'" output.log ||  {
+    echo "FAIL: expected warning was seen."
+    exit 1
+}
