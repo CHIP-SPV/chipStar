a0df5ab1cf309fe5e441f1c58dad356440481501
Paulius Velesko <pvelesko@pglc.io>
2023-06-17 03:03:46 -0500

    fix waitForThreadExit lock order

--- src/CHIPBackend.cc
+++ src/CHIPBackend.cc
@@ -1245,11 +1245,12 @@ void CHIPBackend::waitForThreadExit() {
   // Cleanup all queues
   {
     LOCK(Backend->BackendMtx); // prevent devices from being destrpyed
-    LOCK(Backend->EventsMtx);  // CHIPBackend::Events
 
     for (auto Dev : Backend->getDevices()) {
       Dev->getLegacyDefaultQueue()->updateLastEvent(nullptr);
-      int NumQueues = Dev->getQueues().size();
+      LOCK(Dev->DeviceMtx);  // CHIPBackend::Events
+      LOCK(Backend->EventsMtx);  // CHIPBackend::Events
+      int NumQueues = Dev->getQueuesNoLock().size();
       if (NumQueues) {
         logWarn("Not all user created streams have been destoyed... Queues "
                 "remaining: {}",
@@ -1257,7 +1258,7 @@ void CHIPBackend::waitForThreadExit() {
         logWarn("Make sure to call hipStreamDestroy() for all queues that have "
                 "been created via hipStreamCreate()");
         logWarn("Removing user-created streams without calling a destructor");
-        Dev->getQueues().clear();
+        Dev->getQueuesNoLock().clear();
         if (Backend->Events.size()) {
           logWarn("Clearing Event list {}", Backend->Events.size());
           Backend->Events.clear();
