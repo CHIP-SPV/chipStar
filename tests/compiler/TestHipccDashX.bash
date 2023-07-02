257dcf902ac64c7c848b00ec72dee12302efb3f3
Paulius Velesko <pvelesko@pglc.io>
2023-07-02 22:00:49 +0300

    fix -x hip (#523)
    * fix -x hip
    
    * Update TestHipccDashX.bash

---
+++ tests/compiler/TestHipccDashX.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Test hipcc handling of -x hip
+set -eu
+
+SRC_DIR=@CMAKE_CURRENT_SOURCE_DIR@
+OUT_DIR=@CMAKE_CURRENT_BINARY_DIR@/@TEST_NAME@.d
+HIPCC=@CMAKE_BINARY_DIR@/bin/hipcc.bin
+
+mkdir -p ${OUT_DIR}
+touch source1.xyz
+touch source2.foo
+
+${HIPCC} -x hip ./source1.xyz ./source2.foo -c
+
