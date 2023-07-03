7d25d95cb0e27fdf2848dc80ddd23996ca7354c1
Paulius Velesko <pvelesko@pglc.io>
2023-07-03 06:43:23 -0500

    fix hipcc tests

---
+++ tests/compiler/TestHipccCompileThenLink.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Test the case where object files are compiled separately and then linked 
+set -eu
+
+SRC_DIR=@CMAKE_CURRENT_SOURCE_DIR@
+OUT_DIR=@CMAKE_CURRENT_BINARY_DIR@/@TEST_NAME@.d
+HIPCC=@CMAKE_BINARY_DIR@/bin/hipcc.bin
+
+export HIPCC_VERBOSE=7
+mkdir -p ${OUT_DIR}
+echo "Step #1"
+${HIPCC} -c ${SRC_DIR}/TestHipccCompileThenLinkKernel.cpp -o ${OUT_DIR}/TestHipccCompileThenLinkKernel.cc.o
+echo "Step #2"
+${HIPCC} -c ${SRC_DIR}/TestHipccCompileThenLinkMain.cpp -o ${OUT_DIR}/TestHipccCompileThenLinkMain.cc.o
+echo "Step #3"
+${HIPCC}  ${OUT_DIR}/TestHipccCompileThenLinkKernel.cc.o  ${OUT_DIR}/TestHipccCompileThenLinkMain.cc.o  -o TestHipccCompileThenLink 
