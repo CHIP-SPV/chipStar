5db760d349f24a420a8a0346b6560bc018802ae7
Paulius Velesko <pvelesko@pglc.io>
2023-07-05 08:30:13 +0300

    Fix hipcc tests, compile then link case (#528)
    * fix hipcc tests
    
    * update HIPCC submodule

---
+++ tests/compiler/TestHipccCompileAndLink.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Test the case where there's a mix of object files & source files
+set -eu
+
+SRC_DIR=@CMAKE_CURRENT_SOURCE_DIR@
+OUT_DIR=@CMAKE_CURRENT_BINARY_DIR@/@TEST_NAME@.d
+HIPCC=@CMAKE_BINARY_DIR@/bin/hipcc.bin
+
+export HIPCC_VERBOSE=7
+mkdir -p ${OUT_DIR}
+echo "Step #1"
+${HIPCC} -c ${SRC_DIR}/TestHipccCompileThenLinkKernel.cpp -o TestHipccCompileThenLinkKernel.cc.o
+echo "Step #2"
+${HIPCC}  TestHipccCompileThenLinkKernel.cc.o ${SRC_DIR}/TestHipccCompileThenLinkMain.cpp -o TestHipccCompileAndLink 
