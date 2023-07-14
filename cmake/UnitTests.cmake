7cfae74995405592d77446ccd2711512b0744d9c
Paulius Velesko <pvelesko@pglc.io>
2023-07-14 05:08:00 -0500

    exclude Unit_hipMallocPitch_KernelLaunch tests
    pocl driver segfault

--- cmake/UnitTests.cmake
+++ cmake/UnitTests.cmake
@@ -1859,6 +1859,9 @@ list(APPEND IGPU_LEVEL0_FAILED_TESTS
  # Timeout or out-of-resources error in the CI which emulates double FPs.
 list(APPEND IGPU_LEVEL0_FAILED_TESTS "TestStlFunctionsDouble")
 
+list(APPEND CPU_POCL_FAILED_TESTS "Unit_hipMallocPitch_KernelLaunch - int") # Segfault inside PoCL runtime
+list(APPEND CPU_POCL_FAILED_TESTS "Unit_hipMallocPitch_KernelLaunch - float") # Segfault inside PoCL runtime
+list(APPEND CPU_POCL_FAILED_TESTS "Unit_hipMallocPitch_KernelLaunch - double") # Segfault inside PoCL runtime
 list(APPEND CPU_POCL_FAILED_TESTS "Unit_hipMultiThreadStreams2") # Subprocess aborted
 list(APPEND CPU_POCL_FAILED_TESTS "hipStreamSemantics") # SEGFAULT
 list(APPEND CPU_POCL_FAILED_TESTS "Unit_hipDeviceSynchronize_Positive_Nullstream") # failing for LLVM16
