8ec54f972bab0d3e98f83a9eebee762d3bf9d987
Pekka Jääskeläinen <pekka.jaaskelainen@intel.com>
2023-08-07 11:42:04 +0300

    Pass -cl-std=CL3.0
    At least Intel CPU driver requires it for exposing the
    atomic_* with generic address space pointer args.

--- src/backend/OpenCL/CHIPBackendOpenCL.cc
+++ src/backend/OpenCL/CHIPBackendOpenCL.cc
@@ -1485,7 +1485,7 @@ chipstar::EventMonitor *CHIPBackendOpenCL::createStaleEventMonitor_() {
 }
 
 std::string CHIPBackendOpenCL::getDefaultJitFlags() {
-  return std::string("-x spir -cl-kernel-arg-info");
+  return std::string("-x spir -cl-kernel-arg-info -cl-std=CL3.0");
 }
 
 void CHIPBackendOpenCL::initializeImpl(std::string CHIPPlatformStr,
