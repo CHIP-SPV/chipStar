b2e6d2fbc332ba1034445bfa9197605abe56ebab
Paulius Velesko <pvelesko@pglc.io>
2023-08-15 00:52:26 -0500

    add eventRecord blocking test

---
+++ tests/runtime/TestRecordEventBlocking.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <chrono>
+#include <hip/hip_runtime.h>
+
+// Simple time-consuming kernel without arguments
+__global__ void slowKernel() {
+    float val = 0.0f;
+    for (int i = 0; i < 100000000; i++) {
+        for (int j = 0; j < 10000; j++) {
+            val += sqrtf(val + i + j);
+        }
+    }
+}
+
+int main() {
+    float milliseconds = 0;
+    hipEvent_t start, stop;
+    hipEventCreate(&start);
+    hipEventRecord(start, 0);
+    // hipEventElapsedTime(&milliseconds, start, start);
+    hipLaunchKernelGGL(slowKernel, dim3(512), dim3(256), 0, 0);
+    // time how long it takes to execute to check if blocking or not
+    auto startTime = std::chrono::high_resolution_clock::now();
+    hipEventElapsedTime(&milliseconds, start, start);
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto  ms = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+    if(ms > 1000)
+        std::cout << "Blocking" << std::endl;
+    else
+        std::cout << "Non-Blocking" << std::endl;
+
+    return 0;
+}
