ab234b9064a3ebc2282a60423190edfdf2e3df98
Paulius Velesko <pvelesko@pglc.io>
2023-08-29 12:56:58 +0000

    Adjust TestRecordEventBlocking test to run ~10 seconds on Sunspot

--- tests/runtime/TestRecordEventBlocking.cpp
+++ tests/runtime/TestRecordEventBlocking.cpp
@@ -5,8 +5,8 @@
 // Simple time-consuming kernel without arguments
 __global__ void slowKernel() {
   float val = 0.0f;
-  for (int i = 0; i < 1000000; i++) {
-    for (int j = 0; j < 1000000; j++) {
+  for (int i = 0; i < 10000; i++) {
+    for (int j = 0; j < 100000; j++) {
       val += sqrtf(val + i + j);
     }
   }
