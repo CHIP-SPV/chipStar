8e75279eb67a8ef2924859d8df492d2a1cbcb8ce
Michal Babej <michal.babej@intel.com>
2023-09-04 11:46:15 +0300

    Add missing license

--- llvm_passes/SPIRVImageType.hh
+++ llvm_passes/SPIRVImageType.hh
@@ -1,3 +1,12 @@
+//===- SPIRVImageType.cc -------------------------------------------------===//
+//
+// copied from clang/lib/CodeGen/Targets/SPIR.cpp
+//
+// Originally part of the LLVM Project, under the Apache License v2.0 with
+// LLVM Exceptions. See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 
 enum ImageAccessQualifier : unsigned { AQ_ro = 0, AQ_wo = 1, AQ_rw = 2 };
 
