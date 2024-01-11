b6fc30ebb99d6bf5859116ac33d3320d3a165e31
Paulius Velesko <pvelesko@pglc.io>
2024-01-11 07:54:28 -0600

    use a disk local module

--- scripts/unit_tests.sh
+++ scripts/unit_tests.sh
@@ -104,6 +104,7 @@ export CHIP_LOGLEVEL=err
 export POCL_KERNEL_CACHE=0
 
 # Use OpenCL for building/test discovery to prevent Level Zero from being used in multi-thread/multi-process environment
+module use ~/modulefiles
 module load oneapi/mkl oneapi/compiler/2024.0.0 $CLANG opencl/dgpu
 
 output=$(clinfo -l 2>&1 | grep "Platform #0")
