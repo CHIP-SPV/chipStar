34986ccd2b081d054cd52d1af2c470e8b0fe335b
Paulius Velesko <pvelesko@pglc.io>
2024-02-24 16:35:38 +0200

    skip tests if already present

--- scripts/test_all.sh
+++ scripts/test_all.sh
@@ -23,33 +23,50 @@ done
 source /usr/local/Modules/init/bash
 
 # OpenCL dGPU
-module purge && module load opencl/dgpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_opencl_dgpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_opencl_dgpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_opencl_dgpu_failed.log
+# if alltest_opencl_dgpu_failed.log exists, then skip
+if [ ! -f alltest_opencl_dgpu_failed.log ]; then
+    module purge && module load opencl/dgpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_opencl_dgpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_opencl_dgpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_opencl_dgpu_failed.log
+fi
 
 # OpenCL iGPU
-module purge && module load opencl/igpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_opencl_igpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_opencl_igpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_opencl_igpu_failed.log
+if [ ! -f alltest_opencl_igpu_failed.log ]; then
+    module purge && module load opencl/igpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_opencl_igpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_opencl_igpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_opencl_igpu_failed.log
+fi
 
 # Level Zero dGPU ICL
-module purge && module load level-zero/dgpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0icl_dgpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_dgpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0icl_dgpu_failed.log
+if [ ! -f alltest_level0icl_dgpu_failed.log ]; then
+    module purge && module load level-zero/dgpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0icl_dgpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_dgpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0icl_dgpu_failed.log
+fi
 
 # Level Zero iGPU ICL
-module purge && module load level-zero/igpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0icl_igpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_igpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0icl_igpu_failed.log
+if [ ! -f alltest_level0icl_igpu_failed.log ]; then
+    module purge && module load level-zero/igpu && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0icl_igpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_igpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0icl_igpu_failed.log
+fi
 
 # Level Zero dGPU RCL
-module purge && module load level-zero/dgpu && CHIP_L0_IMM_CMD_LISTS=OFF ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0rcl_dgpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_dgpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0rcl_dgpu_failed.log
+if [ ! -f alltest_level0rcl_dgpu_failed.log ]; then
+    module purge && module load level-zero/dgpu && CHIP_L0_IMM_CMD_LISTS=OFF ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0rcl_dgpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_dgpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0rcl_dgpu_failed.log
+fi
 
 # Level Zero iGPU RCL
-module purge && module load level-zero/igpu && CHIP_L0_IMM_CMD_LISTS=OFF ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0rcl_igpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_igpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0rcl_igpu_failed.log
+if [ ! -f alltest_level0rcl_igpu_failed.log ]; then
+    module purge && module load level-zero/igpu && CHIP_L0_IMM_CMD_LISTS=OFF ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_level0rcl_igpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_level0_igpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_level0rcl_igpu_failed.log
+fi
 
 # OpenCL CPU
-module purge && module load oneapi/compiler/latest && CHIP_BE=opencl CHIP_DEVICE_TYPE=cpu CHIP_PLATFORM=1 ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_opencl_cpu.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_opencl_cpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_opencl_cpu_failed.log
+if [ ! -f alltest_opencl_cpu_failed.log ]; then
+    module purge && module load oneapi/compiler/latest && CHIP_BE=opencl CHIP_DEVICE_TYPE=cpu CHIP_PLATFORM=1 ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_opencl_cpu.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_opencl_cpu.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_opencl_cpu_failed.log
+fi
 
 # OpenCL PoCL
-module purge && module load pocl/5.0 && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_pocl.log
-awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_pocl.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_pocl_failed.log
+if [ ! -f alltest_pocl_failed.log ]; then
+    module purge && module load pocl/5.0 && ctest --timeout 200 -j $num_threads --repeat until-fail:$num_tries -O alltest_pocl.log
+    awk '/The following tests FAILED/,/\[ERROR_MESSAGE\]/' alltest_pocl.log | sed '/\[ERROR_MESSAGE\]/d' > alltest_pocl_failed.log
+fi
