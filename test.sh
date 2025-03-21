fcc0e7d5ac98cacd7fa0bb96aea2e5fae01f44ad
Paulius Velesko <pvelesko@pglc.io>
2025-03-21 12:57:04 +0200

    fix non-std func returns

--- test.sh
+++ test.sh
@@ -7,7 +7,12 @@
 # /space/pvelesko/install/llvm/18.0/bin/opt -load-pass-plugin /space/pvelesko/chipStar/fix-promote-int-pass/build/install/lib/llvm/libLLVMHipSpvPasses.so -passes=hip-post-link-passes $IR -o $IR.bc -debug
 # /space/pvelesko/install/llvm/18.0/bin/llvm-spirv $IR.bc -o $IR.spv
 
-IR=~/chipStar/fix-promote-int-pass/ext-repro.ll
+# IR=~/chipStar/fix-promote-int-pass/ext-repro.ll
+# llvm-as $IR
+# /space/pvelesko/install/llvm/18.0/bin/opt -load-pass-plugin /space/pvelesko/chipStar/fix-promote-int-pass/build/lib/libLLVMHipSpvPasses.so -passes=hip-post-link-passes $IR -o $IR.bc -debug
+# /space/pvelesko/install/llvm/18.0/bin/llvm-spirv $IR.bc -o $IR.spv
+
+IR=~/chipStar/fix-promote-int-pass/func-return-nonstd.ll
 llvm-as $IR
-/space/pvelesko/install/llvm/18.0/bin/opt -load-pass-plugin /space/pvelesko/chipStar/fix-promote-int-pass/build/install/lib/llvm/libLLVMHipSpvPasses.so -passes=hip-post-link-passes $IR -o $IR.bc -debug
+/space/pvelesko/install/llvm/18.0/bin/opt -load-pass-plugin /space/pvelesko/chipStar/fix-promote-int-pass/build/lib/libLLVMHipSpvPasses.so -passes=hip-post-link-passes $IR -o $IR.bc -debug
 /space/pvelesko/install/llvm/18.0/bin/llvm-spirv $IR.bc -o $IR.spv
