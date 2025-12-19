7f744960dc583f0e212b4306110f61a89bc9fac1
Paulius Velesko <pvelesko@pglc.io>
2025-12-19 16:01:29 +0200

    Add debug output and error checks to repro_min

--- level0-repro/repro_min.cpp
+++ level0-repro/repro_min.cpp
@@ -3,28 +3,37 @@
 #include <thread>
 #include <atomic>
 #include <iostream>
+#include <chrono>
 #define C(x) if((x)!=ZE_RESULT_SUCCESS){std::cerr<<"FAIL:"<<#x<<"\n";return 1;}
 
 std::atomic<bool> stop{false};
-ze_event_handle_t evt;
+ze_event_handle_t evt = nullptr;
 
 void syncThread() {
   while (!stop.load()) {
-    zeEventHostSynchronize(evt, 1000);
+    if (evt != nullptr) {
+      zeEventHostSynchronize(evt, 1000);
+    } else {
+      std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    }
   }
 }
 
 int main() {
-  C(zeInit(0));
-  uint32_t dc=0;
-  ze_driver_handle_t drv; C(zeDriverGet(&dc,nullptr)); C(zeDriverGet(&dc,&drv));
-  uint32_t nc=0;
-  ze_device_handle_t dev; C(zeDeviceGet(drv,&nc,nullptr)); C(zeDeviceGet(drv,&nc,&dev));
-  uint32_t sc=0; zeDeviceGetSubDevices(dev,&sc,nullptr);
+  std::cerr<<"1\n"; C(zeInit(0));
+  std::cerr<<"2\n"; uint32_t dc=0;
+  std::cerr<<"3\n"; ze_driver_handle_t drv; C(zeDriverGet(&dc,nullptr)); C(zeDriverGet(&dc,&drv));
+  if(dc==0){std::cerr<<"No drivers\n";return 1;}
+  std::cerr<<"4\n"; uint32_t nc=0;
+  std::cerr<<"5\n"; ze_device_handle_t dev; C(zeDeviceGet(drv,&nc,nullptr));
+  if(nc==0){std::cerr<<"No devices\n";return 1;}
+  std::cerr<<"6\n"; C(zeDeviceGet(drv,&nc,&dev));
+  std::cerr<<"7\n"; uint32_t sc=0; zeDeviceGetSubDevices(dev,&sc,nullptr);
   if(sc>0){ze_device_handle_t sds[16];uint32_t c=sc;zeDeviceGetSubDevices(dev,&c,sds);dev=sds[0];}
-  
+  std::cerr<<"8\n";
   ze_context_desc_t cd={ZE_STRUCTURE_TYPE_CONTEXT_DESC};
-  ze_context_handle_t ctx; C(zeContextCreate(drv,&cd,&ctx));
+  std::cerr<<"9\n"; ze_context_handle_t ctx; C(zeContextCreate(drv,&cd,&ctx));
+  std::cerr<<"10\n";
   
   ze_command_queue_desc_t qd={ZE_STRUCTURE_TYPE_COMMAND_QUEUE_DESC};
   qd.flags=ZE_COMMAND_QUEUE_FLAG_IN_ORDER;
@@ -35,8 +44,14 @@ int main() {
   ze_event_pool_handle_t pool; C(zeEventPoolCreate(ctx,&pd,0,nullptr,&pool));
   
   ze_event_desc_t ed={ZE_STRUCTURE_TYPE_EVENT_DESC};
+  ed.index=0;
   ed.signal=ed.wait=ZE_EVENT_SCOPE_FLAG_HOST;
   C(zeEventCreate(pool,&ed,&evt));
+  if(evt==nullptr){std::cerr<<"Event creation failed\n";return 1;}
+  
+  // Signal once to ensure event is valid
+  C(zeEventHostReset(evt));
+  C(zeEventHostSignal(evt));
   
   std::thread t(syncThread);
   
